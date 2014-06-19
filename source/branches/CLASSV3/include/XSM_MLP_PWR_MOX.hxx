76e72549875497a53083dc81c6710136e7975f85
Baptiste LENIAU <baptiste.leniau@subatech.in2p3.fr>
2014-06-19 13:09:30 +0000

    Adding physicModels XSMOdel
    git-svn-id: svn+ssh://svn.in2p3.fr/class@272 0e7d625b-0364-4367-a6be-d5be4a48d228

---
+++ source/branches/CLASSV3/include/XSM_MLP_PWR_MOX.hxx
@@ -0,0 +1,72 @@
+
+#ifndef _XSM_MLP_PWR_MOX_HXX
+#define _XSM_MLP_PWR_MOX_HXX
+
+
+/*!
+ \file
+ \brief Header file for XSM_MLP_PWR_MOX class.
+ 
+ 
+ @authors BLG
+ @version 1.0
+ */
+#include "XSModel.hxx"
+#include <string>
+#include <fstream>
+#include <iostream> 
+#include <map>
+#include <vector>
+typedef long long int cSecond;
+using namespace std;
+
+//-----------------------------------------------------------------------------//
+/*!
+ Define a XSM_MLP_PWR_MOX.
+This is the class to predict cross sections with a set of MultiLayerPerceptrons
+Design for a PWR MOX reactor
+
+ @authors BLG
+ @version 1.0
+ */
+//________________________________________________________________________
+
+
+class XSM_MLP_PWR_MOX : public XSModel
+{
+public : 
+
+	/*!
+	 \name Constructor/Desctructor
+	 */
+	//@{
+
+	XSM_MLP_PWR_MOX(string TMVA_Weight_Directory,string InformationFile="");	
+
+	//{
+
+
+ 	EvolutionData GetCrossSections(IsotopicVector IV) ;
+
+
+ private :
+    void SetDataBaseInformation(); //<! Read information file and fill HM mass, Power, time vector
+    vector<double> GetMLPTime() {return fMLP_Time; }
+
+ 	void GetMLPWeightFiles();
+    void ReadWeightFile(string Filename, int &Z, int &A, int &I, int &Reaction) ;
+    double ExecuteTMVA(string WeightFile);
+    void CreateTMVAInputTree(IsotopicVector isotopicvector,int TimeStep);
+
+
+ 	vector<double> 	fMLP_Time;
+ 	vector<string> 		fWeightFiles;
+ 	string fTMVAWeightFolder;
+ 	string fMLPInformationFile;
+ 	double fDataBasePower;
+ 	double fDataBaseHMMass;
+
+};
+
+#endif
+
