b4794b71e20e414efddeeed9bc04aceaf9cc7e0e
leniau <leniau@0e7d625b-0364-4367-a6be-d5be4a48d228>
2014-06-30 13:12:57 +0000

    git-svn-id: svn+ssh://svn.in2p3.fr/class@280 0e7d625b-0364-4367-a6be-d5be4a48d228

---
+++ source/branches/CLASSV3/include/EQM_MLP_PWR_MOX.hxx
@@ -0,0 +1,62 @@
+#ifndef _EQM_MLP_MOX_HXX
+#define _EQM_MLP_MOX_HXX
+
+#include "CLASSHeaders.hxx"
+
+/*!
+ \file
+ \brief Header file for EQM_MLP_MOX class.
+ 
+ 
+ @author BaM
+ @version 1.0
+ */
+
+
+using namespace std;
+
+//-----------------------------------------------------------------------------//
+/*!
+ Define a EQM_MLP_MOX.
+ The aim of these class is to constuct a fuel from an equivalence model
+ based on a  Multi layer perceptron
+
+ @author BaM
+ @version 3.0
+ */
+//________________________________________________________________________
+
+class IsotopicVector;
+
+
+class EQM_MLP_MOX  : public EquivalenceModel
+{
+	public :
+	
+	EQM_MLP_MOX(string TMVAWeightPath);
+
+	///  method called to build a reprocessed fuel as a function of the burnup requierement the stock, mass....
+	/*!
+	 Build the fuel following the equivalance model with the proper requierment in term of mass burnup....
+	 \param double BurnUp desireted burnup reached by the fuel at the end of irradiation
+	 \param double HMMass, needed Heavy metal mass needed
+	 \param vector<double> &lambda, fraction of the stock to take (initialy should be 0)
+	 \param vector<IsotopicVector> FissilArray, isotopicvectors to use to get the fissil part of the fuel
+	 \param vector<IsotopicVector> FertilArray, isotopicvectors to use to get the fertil part of the fuel (if empty take it from the god)
+	 */
+	
+	vector<double>  BuildFuel(double BurnUp, double HMMass,vector<IsotopicVector> FissilArray, vector<IsotopicVector> FertilArray = 0);
+	//}
+	
+	private :
+	void CreateTMVAInputTree(IsotopicVector Fissil,IsotopicVector Fertil,double BurnUp);
+	double ExecuteTMVA();
+	void GuessLambda(double& lambda, int& StockID,int FirstStockID, int LastStockID, double DeltaM,double StockHM);
+
+
+	string fTMVAWeightPath;
+
+};
+
+#endif
+
--- source/branches/CLASSV3/include/XSM_MLP_PWR_MOX.hxx
+++ source/branches/CLASSV3/include/XSM_MLP_PWR_MOX.hxx
@@ -41,7 +41,7 @@ public :
 	 */
 	//@{
 
-	XSM_MLP_PWR_MOX(LogFile* Log,string TMVA_Weight_Directory,string InformationFile="");	
+	XSM_MLP_PWR_MOX(LogFile* Log,string TMVA_Weight_Directory,string InformationFile="",bool IsTimeStep=true);	
 	~XSM_MLP_PWR_MOX() {;}
 	//{
 
@@ -57,6 +57,13 @@ public :
  	void ReadWeightFile(string Filename, int &Z, int &A, int &I, int &Reaction) ;
  	double ExecuteTMVA(string WeightFile);
  	void CreateTMVAInputTree(IsotopicVector isotopicvector,int TimeStep);
+ 	EvolutionData GetCrossSectionsTime(IsotopicVector IV) ;
+
+ 	void ReadWeightFileStep(string Filename, int &Z, int &A, int &I, int &Reaction, int &TimeStep) ;
+ 	double ExecuteTMVAStep(string WeightFile);
+ 	void CreateTMVAInputTreeStep(IsotopicVector isotopicvector);
+ 	EvolutionData GetCrossSectionsStep(IsotopicVector IV) ;
+
 
 
  	vector<double> 	fMLP_Time;
@@ -65,6 +72,7 @@ public :
  	string fMLPInformationFile;
  	double fDataBasePower;
  	double fDataBaseHMMass;
+ 	bool fIsStepTime;
 
 };
 
