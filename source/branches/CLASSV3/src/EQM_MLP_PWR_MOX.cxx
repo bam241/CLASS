c1959e11a6991a7d0e018185e416e0dbf07686f9
Baptiste LENIAU <baptiste.leniau@subatech.in2p3.fr>
2014-07-01 14:39:15 +0000

    git-svn-id: svn+ssh://svn.in2p3.fr/class@289 0e7d625b-0364-4367-a6be-d5be4a48d228

--- source/branches/CLASSV3/src/EQM_MLP_PWR_MOX.cxx
+++ source/branches/CLASSV3/src/EQM_MLP_PWR_MOX.cxx
@@ -1,3 +1,4 @@
+#include "EquivalenceModel.hxx"
 #include "EQM_MLP_PWR_MOX.hxx"
 #include "LogFile.hxx"
 #include "StringLine.hxx"
@@ -13,9 +14,9 @@
 #include "TMVA/Reader.h"
 #include "TMVA/Tools.h"
 #include "TMVA/MethodCuts.h"
-#include "TFile.h"
 
-#include "IsotopicVector.hxx"
+
+#include "CLASSHeaders.hxx"
 
 //________________________________________________________________________
 //
@@ -30,8 +31,6 @@ EQM_MLP_MOX::EQM_MLP_MOX(string TMVAWeightPath)
 {
 	fTMVAWeightPath =  TMVAWeightPath;
 	
-
-
 	ZAI U8(92,238,0);
 	ZAI U5(92,235,0);
 	double U5_enrich= 0.0025;
@@ -42,16 +41,14 @@ EQM_MLP_MOX::EQM_MLP_MOX(string TMVAWeightPath)
 	ZAI Pu1(94,241,0);
 	ZAI Pu2(94,242,0);
 
-	fFissileList =  Pu8*1+Pu9*1+Pu0*1+Pu1*1+Pu2*1;
+	fFissileList = Pu8*1+Pu9*1+Pu0*1+Pu1*1+Pu2*1;
 	fFertileList = U5*U5_enrich + U8*(1-U5_enrich);
+	
 }
-
 //________________________________________________________________________
-void EQM_MLP_MOX::CreateTMVAInputTree(IsotopicVector Fissil,IsotopicVector Fertil,double BurnUp)
+TTree* EQM_MLP_MOX::CreateTMVAInputTree(IsotopicVector Fissil,IsotopicVector Fertil,double BurnUp)
 {
 	/******Create Input data tree to be interpreted by TMVA::Reader***/
-
-	TFile*   InputFile = new TFile("./EQMTMP.root","RECREATE");
 	TTree*   InputTree = new TTree("EQTMP", "EQTMP");
 	float Pu8   			 = 0;
 	float Pu9   			 = 0;
@@ -70,7 +67,6 @@ void EQM_MLP_MOX::CreateTMVAInputTree(IsotopicVector Fissil,IsotopicVector Ferti
 	InputTree->Branch(	"Am1"	,&Am1	,"Am1/F"	);
 	InputTree->Branch(	"U5_enrichment"	,&U5_enrichment	,"U5_enrichment/F"	);
 	InputTree->Branch(	"BU"	,&BU	,"BU/F"	);
-
 
 
 	float U8     = Fertil.GetZAIIsotopicQuantity(92,238,0);
@@ -109,13 +105,10 @@ void EQM_MLP_MOX::CreateTMVAInputTree(IsotopicVector Fissil,IsotopicVector Ferti
 
 	InputTree->Fill();
 
-	InputFile->Write();
-	delete InputTree;
-	InputFile-> Close();
-	delete InputFile;
+	return InputTree;
 }
 //________________________________________________________________________
-double EQM_MLP_MOX::ExecuteTMVA()
+double EQM_MLP_MOX::ExecuteTMVA(TTree* theTree)
 {
 	// --- Create the Reader object
 
@@ -139,18 +132,6 @@ double EQM_MLP_MOX::ExecuteTMVA()
 	TString methodName = "MLP method";
 	reader->BookMVA( methodName, fTMVAWeightPath );
 
-	// Prepare input tree
-	TFile *input(0);
-	if (!gSystem->AccessPathName( "./EQMTMP.root" )) {
-		input = TFile::Open( "./EQMTMP.root" ); // check if file in local directory exists
-	}
-	if (!input) {
-		std::cout << "ERROR: could not open data file" << std::endl;
-		exit(1);
-	}
-
-	TTree* theTree = (TTree*)input->Get("EQTMP");
-
 	theTree->SetBranchAddress("teneur",&teneur);
 	theTree->SetBranchAddress( "Pu8"  ,&Pu8   );
 	theTree->SetBranchAddress( "Pu9"  ,&Pu9   );
@@ -165,140 +146,11 @@ double EQM_MLP_MOX::ExecuteTMVA()
 
 	delete reader;
 	delete theTree;
-	input->Close();
-	delete input;
-	//cout<<"....done"<<endl;
-
 	//cout<<val<<endl;
 	return (double)val; //retourne teneur
 }
 //________________________________________________________________________
-void EQM_MLP_MOX::GuessLambda(double& lambda, int& StockID,int FirstStockID, int LastStockID, double DeltaM,double StockHM)
+double EQM_MLP_MOX::GetFissileMolarFraction(IsotopicVector Fissil,IsotopicVector Fertil,double BurnUp)
 {
-
-	double Threshold = 50 ; //50 grams 
-
-
-	/****Initialization***/
-	if(lambda==0) 
-		lambda=0.5;
-	
-	/********dichotomie**************/
-	if(DeltaM>0)  //MassNeeded - AvailableMass
-	{
-		lambda+=lambda/2.;
-
-		if(lambda >= 1) //this stock is not enought go to next one
-		{
-			lambda = 1;
-
-		if( !( StockID+1 == LastStockID) ) //if its the last stock don't try to look the next one (segfault otherwize)
-				StockID++;
-		}
-
-	}
-
-	else if(DeltaM<0) 
-	{
-		lambda-=lambda/2.;
-
-		if(lambda*StockHM < Threshold) //if only 50 grams left in actual stock go to previous one
-		{
-			lambda=0;
-			StockID--;
-			if(StockID<FirstStockID)
-			{
-				cout<<"Critical error  EQM_MLP_MOX::GuessLambda"<<endl;
-				cout<<"Contact BLG"<<endl;
-				exit(1);
-			}	
-
-		}
-
-	}
-
+	return	ExecuteTMVA(CreateTMVAInputTree(Fissil,Fertil,BurnUp));
 }
-
-//________________________________________________________________________
-vector<double> EQM_MLP_MOX::BuildFuel(double BurnUp, double HMMass,vector<IsotopicVector> FissilArray, vector<IsotopicVector> FertilArray)
-{
-
-	HMMass*=1e6;//tons to gram
-			
-	vector<double> lambda ; //vector of portion of stocks taken (fissile & fertil)
-	for(int i=0;FissilArray.size() + FertilArray.size();i++ );
-		lambda.push_back(0);
-
-		IsotopicVector DepletedUranium;
-		IsotopicVector PlutoniumVector;
-		int StockID = 0 ;
-		double AvailablePuMass=0;
-		double PuMassNeeded=1000; //At present time I have no clue what is the requiered Pu mass, I assume at least 1kg is needed 
-		double WeightPuContent=0;
-		double MassPrecision=100; //Mass precision is 100 grams
-		while(  abs(PuMassNeeded - AvailablePuMass) > MassPrecision )
-		{
-			//Increase the portion of the stock stokID taken, according to the followings variables
-			double DeltaM=PuMassNeeded-AvailablePuMass;
-			GuessLambda( lambda[StockID], StockID,0,FissilArray.size()-1, DeltaM, FissilArray[StockID].GetTotalMass() * 1e6 );
-
-			//Build the Plutonium vector from stocks
-			PlutoniumVector.Clear();
-			for(int i=0;i<=StockID;i++) 
-				PlutoniumVector += lambda[i] * FissilArray[i];
-
-			AvailablePuMass = PlutoniumVector.GetTotalMass() * 1e6; //in grams
-
-			//Build uranium vector from stocks
-			int FertileStockID = FissilArray.size();
-			double FertilMassNeeded = HMMass - AvailablePuMass;
-			double AvailableFertilMass = 0;
-
-			while( abs(FertilMassNeeded - AvailableFertilMass) > MassPrecision  )
-			{	
-				double DeltaM=FertilMassNeeded-AvailableFertilMass;
-				GuessLambda( lambda[FertileStockID], FertileStockID,FissilArray.size(),FertilArray.size()-1, DeltaM, FertilArray[FertileStockID-FissilArray.size()].GetTotalMass() * 1e6 ) ;
-
-				int j=-1;
-				DepletedUranium.Clear();
-				for(int i=int(FissilArray.size());i<=FertileStockID;i++)
-				{	DepletedUranium += lambda[i] * FertilArray[j];
-					j++;
-				}	
-				AvailableFertilMass=DepletedUranium.GetTotalMass() * 1e6; //in grams
-
-				if( j+1 == int( FertilArray.size() ) && FertilMassNeeded  > AvailableFertilMass ) //if this is the last stock and it's not enought
-				{
-					cout<<"You requiere more depleted uranium "<<"("<<DeltaM/1e6<<" t needed) ! Reactor not fill"<<endl;
-					for(int i=0 ; i<int(lambda.size()) ; i++)
-						lambda[i]=-1;
-					break;
-				}
-
-			}	
-			/*Calcul the quantity of this composition needed to reach the burnup*/
-			CreateTMVAInputTree(PlutoniumVector,DepletedUranium,BurnUp);
-			double MolarPuContent = ExecuteTMVA();
-			system("rm EQMTMP.root ");
-
-			double MeanMolarPu = PlutoniumVector.MeanMolar();
-			double MeanMolarDepletedU = DepletedUranium.MeanMolar();
-			double MeanMolar   = MeanMolarPu*MolarPuContent + (1-MolarPuContent)*MeanMolarDepletedU;
-
-			WeightPuContent = MolarPuContent * MeanMolarPu/MeanMolar ;
-
-			PuMassNeeded = WeightPuContent  *  HMMass ;
-
-			if( StockID+1 == int( FissilArray.size() ) && PuMassNeeded  > AvailablePuMass ) //if this is the last stock and it's not enought
-			{
-				cout<<"You requiere more (or better) plutonium !! Reactor not fill"<<endl;
-				for(int i=0 ; i<int(lambda.size()) ; i++)
-					lambda[i]=-1;
-				break;
-			}
-
-		}
-
-return lambda;
-}
-//________________________________________________________________________
