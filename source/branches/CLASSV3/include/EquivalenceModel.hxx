77b0ec79013ad561b01073793402d511d80e6400
Baptiste LENIAU <baptiste.leniau@subatech.in2p3.fr>
2014-07-09 12:09:58 +0000

    git-svn-id: svn+ssh://svn.in2p3.fr/class@319 0e7d625b-0364-4367-a6be-d5be4a48d228

--- source/branches/CLASSV3/include/EquivalenceModel.hxx
+++ source/branches/CLASSV3/include/EquivalenceModel.hxx
@@ -39,9 +39,9 @@ class EquivalenceModel : public CLASSObject
 
 	virtual ~EquivalenceModel();
 
-	/// virtueal method called to build a reprocessed fuel as a function of the burnup requierement the stock, mass....
+	/// virtual method called to build a reprocessed fuel as a function of the burnup requierement the stock, mass....
 	/*!
-	 Build the fuel following the equivalance model with the proper requierment in term of mass burnup....
+	 Build the fuel following the equivalance model with the proper requierment in term of mass, burnup....
 	 \param double BurnUp desireted burnup reached by the fuel at the end of irradiation
 	 \param double HMMass, needed Heavy metal mass needed
 	 \param vector<double> &lambda, fraction of the stock to take (initialy should be 0)
@@ -52,15 +52,15 @@ class EquivalenceModel : public CLASSObject
 	virtual	 vector<double> BuildFuel(double BurnUp, double HMMass, vector<IsotopicVector> FissilArray, vector<IsotopicVector> FertilArray );
 	//}
 	virtual void GuessLambda(vector<double>& lambda,int FirstStockID, int LastStockID, double DeltaM, vector<IsotopicVector> Stocks, double  HMMass);
-	virtual double GetFissileMolarFraction(IsotopicVector Fissil,IsotopicVector Fertil,double BurnUp) {return 0;}
+	virtual double GetFissileMolarFraction(IsotopicVector Fissil,IsotopicVector Fertil,double BurnUp) {return 0;} //!< Return the molar fraction of fissile element in the fuel accodring to the Burnup, and a given fuel composition (this is the heart of the equivalence model) 
 
 	
 
-	IsotopicVector GetFertileList() {return fFertileList;}
-	IsotopicVector GetFissileList() {return fFissileList;}
+	IsotopicVector GetFertileList() {return fFertileList;}	//!<return the fertile list
+	IsotopicVector GetFissileList() {return fFissileList;}	//!<return the fissile list
 
-	void SetFertileList(IsotopicVector IV) {fFertileList = IV;}
-	void SetFissileList(IsotopicVector IV) {fFissileList = IV;}
+	void SetFertileList(IsotopicVector IV) {fFertileList = IV;}//!<set the fertile list
+	void SetFissileList(IsotopicVector IV) {fFissileList = IV;}//!<set the fissile list
 
 	
 	protected :
@@ -69,15 +69,15 @@ class EquivalenceModel : public CLASSObject
 					//!< if no stock are provided, will take the isotopic vector in the Park income
 
 	IsotopicVector fFissileList;	//!< contain the list of zai, needed as fissile, taken in a stock before fabrication
-					//!< if no stock are provided, will take the isotopic vector in the Park income
+					//!< if no stock are provided the fuel will not be made
 
 
 	private :
 
-	void SetLambda(vector<double>& lambda ,int FirstStockID, int LastStockID, double LAMBDA_TOT);
-	void FindLambdaMax(int FirstStockID, int LastStockID, vector<IsotopicVector> Stocks, double  HMMass);
-	double fOld_Lambda_Tot[2];
-	double fLambda_max[2];
+	void SetLambda(vector<double>& lambda ,int FirstStockID, int LastStockID, double LAMBDA_TOT);	//!< Set individual lambda according to the LAMBDA_TOT (lambda of all stocks)
+	double FindLambdaMax( vector<IsotopicVector> Stocks, double  HMMass);							//!< Find the maximum LAMBDA_TOT of Stocks (ie lambda to reach HMass)
+	double fOld_Lambda_Tot;//!<The old (old iteration) guessed lambda_tot (guessed from GuessLambda)
+	double fLambda_max;//!<Value calculated by FindLambdaMax
 
 };
 
