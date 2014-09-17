63a10bb0e401c99edd59a230acd4742e7c874700
mouginot <mouginot@0e7d625b-0364-4367-a6be-d5be4a48d228>
2014-09-17 13:26:12 +0000

    mv THU/THPu in Utils…. for the moment….
    git-svn-id: svn+ssh://svn.in2p3.fr/class@386 0e7d625b-0364-4367-a6be-d5be4a48d228

---
+++ Utils/trunk/PWR_THPU_FabricationPlant.hxx
@@ -0,0 +1,45 @@
+#ifndef __PWR_THPU_FabricationPlant_HXX__
+#define __PWR_THPU_FabricationPlant_HXX__
+
+/*!
+ \file 
+ \brief Header file for FabricationPlant class.
+
+ The aim of the Class is to manage evolution of FabricationPlant
+
+ 
+ @author BaM, Marc
+ @version 2.0
+ */
+
+
+
+#include <vector>
+#include <map>
+
+#include "FabricationPlant.hxx"
+
+using namespace std;
+typedef long long int cSecond;
+
+class PWR_THPU_FabricationPlant : public FabricationPlant
+{
+	//on utilise les constructeur de la classe FabricationPlant
+	// dans le code :MyFabricationPlant *FP_MOX = new FabricationPlant(gCLASS.GetLog(),Stock, ReUsable);
+	//on a besoin de surcherger le constructeur que si
+public :
+	PWR_THPU_FabricationPlant();
+	PWR_THPU_FabricationPlant(CLASSLogger* log);
+	
+	PWR_THPU_FabricationPlant(CLASSLogger* log, Storage* storage, Storage* reusable, double fabricationtime = 365.25*24*3600*2);
+	///< Normal Destructor.
+	~PWR_THPU_FabricationPlant();
+	
+	void	BuildFuelForReactor(int ReactorId);			//obligatoire, c'est un peu la base
+protected :
+//********* Private Method *********//
+// mettre ici les méthodes dont on a besoin dans sa fonction BuildFuelForReactor perso
+
+};
+
+#endif
