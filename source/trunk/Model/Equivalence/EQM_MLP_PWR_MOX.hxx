53032eb65280dffb388d25601a34383733d96f20
Baptiste Mouginot <mouginot.baptiste@gmail.com>
2015-01-22 09:03:26 +0000

    pdating source: step 2a copying the new soruce : Model
    git-svn-id: svn+ssh://svn.in2p3.fr/class@476 0e7d625b-0364-4367-a6be-d5be4a48d228

---
+++ source/trunk/Model/Equivalence/EQM_MLP_PWR_MOX.hxx
@@ -0,0 +1,50 @@
+#ifndef _EQM_MLP_MOX_HXX
+#define _EQM_MLP_MOX_HXX
+
+#include "EquivalenceModel.hxx"
+#include "TTree.h"
+
+/*!
+ \file
+ \brief Header file for EQM_MLP_MOX class.
+
+
+ @author BaM
+ @version 1.0
+ */
+
+
+using namespace std;
+
+//-----------------------------------------------------------------------------//
+/*!
+ Define a EQM_MLP_MOX.
+ The aim of these class is to constuct a fuel from an equivalence model
+ based on a  Multi layer perceptron
+
+ @author BaM
+ @version 3.0
+ */
+//________________________________________________________________________
+
+
+class EQM_MLP_MOX : public EquivalenceModel
+{
+	public :
+
+	EQM_MLP_MOX(string TMVAWeightPath);	//!< Constructor  string TMVAWeightPath => PATH/TMVAWeight.xml (path to tmva weight)
+	EQM_MLP_MOX(CLASSLogger* log, string TMVAWeightPath);	//!< Constructor CLASSLogger* log ,string TMVAWeightPath => PATH/TMVAWeight.xml
+
+	virtual double GetFissileMolarFraction(IsotopicVector Fissil,IsotopicVector Fertil,double BurnUp); //!<Return the molar fraction of fissile element thanks to a Multi Layer Perceptron
+
+	private :
+	TTree* CreateTMVAInputTree(IsotopicVector Fissil,IsotopicVector Fertil,double BurnUp);//!<Create input tmva tree to be read by ExecuteTMVA
+	double ExecuteTMVA(TTree* theTree);//!<Execute the MLP according to the input tree created by CreateTMVAInputTree
+
+
+	string fTMVAWeightPath;;//!<The weight needed by TMVA to construct and execute the multilayer perceptron
+
+};
+
+#endif
+
---
+++ source/trunk/Model/Equivalence/EQM_QUAD_PWR_MOX.hxx
@@ -0,0 +1,48 @@
+#ifndef _EQM_QUAD_PWR_MOX_HXX
+#define _EQM_QUAD_PWR_MOX_HXX
+
+#include "EquivalenceModel.hxx"
+
+#include <string>
+
+/*!
+ \file
+ \brief Header file for EQM_MLP_MOX class.
+
+
+ @author BaM
+ @version 1.0
+ */
+
+using namespace std;
+
+//-----------------------------------------------------------------------------//
+/*!
+ Define a EQM_MLP_MOX.
+ The aim of these class is to constuct a fuel from an equivalence model
+ based on a Quadratic Pu equivalent Model
+
+ @author BaM
+ @version 3.0
+ */
+//________________________________________________________________________
+
+class EQM_QUAD_PWR_MOX : public EquivalenceModel
+{
+	public :
+
+	EQM_QUAD_PWR_MOX(string WeightPath);
+	EQM_QUAD_PWR_MOX(CLASSLogger* log, string WeightPath);
+	~EQM_QUAD_PWR_MOX();
+
+	virtual double GetFissileMolarFraction(IsotopicVector Fissil,IsotopicVector Fertil,double BurnUp);
+
+	private :
+
+	string fWeightPath;
+	vector<double> fFuelParameter;
+
+};
+
+#endif
+
