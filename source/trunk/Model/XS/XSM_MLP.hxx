fadd318afbdf29c8351e78a7a42d687bbfc6bc2b
Baptiste LENIAU <baptiste.leniau@subatech.in2p3.fr>
2015-01-29 11:42:23 +0000

    change constructor default value
    git-svn-id: svn+ssh://svn.in2p3.fr/class@486 0e7d625b-0364-4367-a6be-d5be4a48d228

--- source/trunk/Model/XS/XSM_MLP.hxx
+++ source/trunk/Model/XS/XSM_MLP.hxx
@@ -48,7 +48,7 @@ class XSM_MLP : public XSModel
 	 \param bool IsTimeStep, if true , one TMVA weihgt per step time is requiered otherwise it assumes time is part of the MLP inputs
 
 	 */
-	XSM_MLP(string TMVA_Weight_Directory,string InformationFile="",bool IsTimeStep=true);
+	XSM_MLP(string TMVA_Weight_Directory,string InformationFile="",bool IsTimeStep=false);
 	XSM_MLP(CLASSLogger* Log,string TMVA_Weight_Directory,string InformationFile="",bool IsTimeStep=false);
 	~XSM_MLP();
 	//{
