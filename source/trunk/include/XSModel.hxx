5bfe10d4d0d592ec3ff718c851166f0b6cc02dc4
leniau <leniau@0e7d625b-0364-4367-a6be-d5be4a48d228>
2015-01-30 13:45:46 +0000

    add comments
    git-svn-id: svn+ssh://svn.in2p3.fr/class@515 0e7d625b-0364-4367-a6be-d5be4a48d228

--- source/trunk/include/XSModel.hxx
+++ source/trunk/include/XSModel.hxx
@@ -21,15 +21,13 @@ class IsotopicVector;
 //-----------------------------------------------------------------------------//
 /*!
  Define a XS Interpolator 
-This is the class for method related to XS prediction
+This is the class for methods related to XS prediction
 
-see @../Model/XS/XSM_CLOSEST.hxx 
-see @../Model/XS/XSM_MLP.hxx 
-...
+\warning
+ Never instantiate XSModel in your CLASS input but it's derivated class
 
-!!!!!!!!!!!!!!!!CAUTION!!!!!!!!!!!!!
-Never instantiate XSModel in your CLASS input but it's derivated class
-
+ @see XSM_CLOSEST
+ @see XSM_MLP
 
  @author BLG
  @version 1.0
@@ -42,24 +40,47 @@ class XSModel : public CLASSObject
 
 	public : 
 
-	XSModel();
-	XSModel(CLASSLogger* log);
+	/*!
+	 \name Constructor/Desctructor
+	 */
+	//@{
+
+	XSModel(); //!<Default constructor
+	
+	
+	XSModel(CLASSLogger* log); //!<Logger constructor
+
+	//@}
+	
+	
+	/*!
+	 \name Virtual methods : This following methods are overloaded in the derivated classes : XSM_CLOSEST & XSM_MLP & ...
+	 */
+	//@{
+	
+	//{
 	/// Pure virtual method called to estimates mean cross sections.
 	/*!
 	 Estimates the mean cross sections evolution according the fresh fuel composition
-	 \param vector<IsotopicVector> IV, Fresh fuel composition
-	 \param double t =0, can be used in XSM_CLOSEST to recalculate 
-	 distance between IV and DataBase at each time step (deprecated)
+	 \param IV : Fresh fuel composition
+	 \param t : deprecated parameter :
+	 \deprecated t : XS update time (used in XSM_Closest)
 	 */
+	//}
 	virtual  EvolutionData GetCrossSections(IsotopicVector IV,double t=0) = 0 ;
 	
 	/// Check either the IsotopicVector IV is in the validity domain of the models.
 	/*!
 	 return true if IV is in ValidityDomain
 	 return false + a warning if IV is not in ValidityDomain
-	 \param vector<IsotopicVector> IV, Fresh fuel composition
+	 \param IsotopicVector IV, Fresh fuel composition
+	 \param t : deprecated parameter :
+	 \deprecated t : XS update time (used in XSM_Closest)
 	 */
 	virtual  bool isIVInDomain(IsotopicVector IV) ;
+	//@}
+	
+	
 	
 	void SetZAIThreshold(int Z_Threshold){fZAIThreshold = Z_Threshold;}//!< Set the Z threshold : ZAI with Z < fZAIThreshold are not manage by CLASS
 	int  GetZAIThreshold(){return fZAIThreshold;}//!< Get the Z threshold
