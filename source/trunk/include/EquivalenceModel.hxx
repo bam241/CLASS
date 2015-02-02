867480ef270e13fd406c938b6004b5a8b45cc548
mouginot <mouginot@0e7d625b-0364-4367-a6be-d5be4a48d228>
2015-02-02 09:46:48 +0000

    adding comments
    git-svn-id: svn+ssh://svn.in2p3.fr/class@524 0e7d625b-0364-4367-a6be-d5be4a48d228

--- source/trunk/include/EquivalenceModel.hxx
+++ source/trunk/include/EquivalenceModel.hxx
@@ -21,17 +21,17 @@ using namespace std;
 //-----------------------------------------------------------------------------//
 /*!
  Define a EquivalenceModel.
- The aim of these class is synthetyse all the commum properties to all 
+ The aim of these class is synthetyse all the commum properties to all
  Equivalence Model.
-
-\warning
-Never instantiate EquivalenceModel in your CLASS input but it's derivated class
-@see EQM_BakerRoss_FBR_MOX
-@see EQM_LIN_PWR_MOX
-@see EQM_MLP_PWR_MOX
-@see EQM_POL_PWR_UO2
-@see EQM_QUAD_PWR_MOX
-
+ 
+ \warning
+ Never instantiate EquivalenceModel in your CLASS input but it's derivated class
+ @see EQM_BakerRoss_FBR_MOX
+ @see EQM_LIN_PWR_MOX
+ @see EQM_MLP_PWR_MOX
+ @see EQM_POL_PWR_UO2
+ @see EQM_QUAD_PWR_MOX
+ 
  @author BLG,BaM
  @version 3.0
  */
@@ -41,14 +41,14 @@ Never instantiate EquivalenceModel in your CLASS input but it's derivated class
 class EquivalenceModel : public CLASSObject
 {
 	public :
-
+	
 	EquivalenceModel();
 	EquivalenceModel(CLASSLogger* log);
-
+	
 	virtual ~EquivalenceModel();
-    //{
-    /// BuildFuel function.
-    /*!
+	//{
+	/// BuildFuel function.
+	/*!
 	 Build the fuel following the equivalance model with the proper requierment in term of mass, burnup....
 	 \param double burnup reached by the fuel at the end of irradiation
 	 \param double HMMass, Heavy metal mass needed
@@ -58,37 +58,41 @@ class EquivalenceModel : public CLASSObject
 	 */
 	virtual	 vector<double> BuildFuel(double BurnUp, double HMMass, vector<IsotopicVector> FissilArray, vector<IsotopicVector> FertilArray );
 	//}
-	virtual void GuessLambda(vector<double>& lambda,int FirstStockID, int LastStockID, double DeltaM, vector<IsotopicVector> Stocks, double  HMMass); //!< Guess a portion of IsotopicVectors to take (dichotomy)
+	
+	virtual void GuessLambda(vector<double>& lambda,
+				 int FirstStockID, int LastStockID, double DeltaM,
+				 vector<IsotopicVector> Stocks, double  HMMass); //!< Guess a portion of IsotopicVectors to take (dichotomy)
+	
 	virtual double GetFissileMolarFraction(IsotopicVector Fissil,IsotopicVector Fertil,double BurnUp) = 0; //!< Return the molar fraction of fissile element in the fuel according to the burnup, and a given fuel composition (this is the heart of the equivalence model)
 	
 	void SetBuildFuelFirstGuess(double FirstGuess){fFirstGuessFissilContent = FirstGuess;} //!<set the initialization value for BuildFuel algorithm
 	double GetBuildFuelFirstGuess(){return fFirstGuessFissilContent;} //!<Get the initialization value for BuildFuel algorithm
 	
-
+	
 	IsotopicVector GetFertileList() {return fFertileList;}	//!<return the fertile list
 	IsotopicVector GetFissileList() {return fFissileList;}	//!<return the fissile list
-
+	
 	void SetFertileList(IsotopicVector IV) {fFertileList = IV;}//!<set the fertile list
 	void SetFissileList(IsotopicVector IV) {fFissileList = IV;}//!<set the fissile list
-
+	
 	
 	protected :
-
+	
 	IsotopicVector fFertileList;	//!< contain the list of zai, needed as fertile, taken in a stock before fabrication
-					//!< if no stock are provided, will take the isotopic vector in the Park income
-
+	//!< if no stock are provided, will take the isotopic vector in the Park income
+	
 	IsotopicVector fFissileList;	//!< contain the list of zai, needed as fissile, taken in a stock before fabrication
-					//!< if no stock are provided the fuel will not be made
-
+	//!< if no stock are provided the fuel will not be made
+	
 	double fFirstGuessFissilContent;//!< fissile content for BuildFuel initialization (in weight proportion)
-
+	
 	private :
-
+	
 	void SetLambda(vector<double>& lambda ,int FirstStockID, int LastStockID, double LAMBDA_TOT);	//!< Set individual lambda according to the LAMBDA_TOT (lambda of all stocks)
 	double FindLambdaMax( vector<IsotopicVector> Stocks, double  HMMass);							//!< Find the maximum LAMBDA_TOT of Stocks (ie lambda to reach HMass)
 	double fOld_Lambda_Tot;//!<The old (old iteration) guessed lambda_tot (guessed from GuessLambda)
 	double fLambda_max;//!<Value calculated by FindLambdaMax
-
+	
 };
 
 #endif
