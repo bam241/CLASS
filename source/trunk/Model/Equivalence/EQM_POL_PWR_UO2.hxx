67a12a0c0449049cc4dfca6b61285a679ebb8ea5
Baptiste LENIAU <baptiste.leniau@subatech.in2p3.fr>
2015-02-04 11:20:18 +0000

    add comments
    git-svn-id: svn+ssh://svn.in2p3.fr/class@532 0e7d625b-0364-4367-a6be-d5be4a48d228

--- source/trunk/Model/Equivalence/EQM_POL_PWR_UO2.hxx
+++ source/trunk/Model/Equivalence/EQM_POL_PWR_UO2.hxx
@@ -5,10 +5,16 @@
 
 using namespace std;
 
-//−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−//
+//−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−--------------−−−−−−−−−−−−−−−//
 /*!
  Define a EQM_POL_PWR_UO2
- Explain briefly what is it . @author BaM
+ It returns the @f$^{235}U@f$ enrichment e according to this polynom :
+ 
+ @f$e=\alpha_{0} + \alpha_{1}\cdotBU + \alpha_{2}\cdotBu\cdot\BU@f$
+ 
+ BU : Maximum achievable burnup
+ 
+ @author BaM
  @version 3.0
  */
 //________________________________________________________________________
@@ -19,15 +25,44 @@ class EQM_POL_PWR_UO2 : public EquivalenceModel
 {
 	
 public:
-	/*Constructor*/
-	EQM_POL_PWR_UO2( ); 
+	/*!
+	 \name Constructor
+	 */
+	//@{
+	
+	//{
+	/// normal constructor
+	/*!
+	 Create a EQM_POL_PWR_UO2
+	 \param  PathToWeightFile :  Path to the file containing the  @f$\alpha_{i}@f$
+	 Format :  @f$PARAM@f$  @f$\alpha_{0}@f$  @f$\alpha_{1}@f$  @f$\alpha_{2}@f$
+	 */
+	EQM_POL_PWR_UO2(string PathToWeightFile);
+	//}
+	
+	//{
+	/// logger constructor
+	/*!
+	 Create a EQM_POL_PWR_UO2
+	 \param log : Use for the log
+	 \param  PathToWeightFile :  Path to the file containing the  @f$\alpha_{i}@f$
+	 Format :  @f$PARAM@f$  @f$\alpha_{0}@f$  @f$\alpha_{1}@f$  @f$\alpha_{2}@f$
+	 */
+	EQM_POL_PWR_UO2(CLASSLogger* log, string PathToWeightFile);
+	//}
 	
 	/**This function IS the equivalence model**/
 	double GetFissileMolarFraction(IsotopicVector Fissil, IsotopicVector Fertil,double BurnUp) ; // !<Return the molar fraction of fissile element
 	
-private:
-	/*Your private variables */
 	
+private:
+	
+	void ReadWeightFile(string PathToWeightFile); //!< Function to read the weight file & file the parameters
+	
+	double fParam_Bu_0 ;		//!<  @f$\alpha_{0}@f$
+	double fParam_Bu ;		//!<  @f$\alpha_{1}@f$
+	double fParam_BuSquare ;	//!<  @f$\alpha_{2}@f$
+
 };
 
 #endif
