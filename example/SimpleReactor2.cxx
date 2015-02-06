39ac38776798fa689dc5338a510651cfdbd778f7
Baptiste LENIAU <baptiste.leniau@subatech.in2p3.fr>
2015-02-06 13:08:12 +0000

    Set a name to output files (.root)
    git-svn-id: svn+ssh://svn.in2p3.fr/class@559 0e7d625b-0364-4367-a6be-d5be4a48d228

--- example/SimpleReactor2.cxx
+++ example/SimpleReactor2.cxx
@@ -36,6 +36,7 @@ int main(int argc, char** argv)
 	Scenario *gCLASS=new Scenario(1977*year,Logger);
 	gCLASS->SetStockManagement(true);//If false all the IsotopicVector in stocks are mixed together.
 	gCLASS->SetTimeStep(year/4.);//the scenario calculation is updated every 3 months
+	gCLASS−>SetOutputFileName("SimpleReactor2.root");	//Set the name of the output file
 
 	/******DATA BASES**********************************/
 	/*===Decay data base===*/
