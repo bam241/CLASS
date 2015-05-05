679e6e2a7ceb1076b1fe11a08089a8be8eae3d58
mouginot <mouginot@0e7d625b-0364-4367-a6be-d5be4a48d228>
2015-05-05 12:53:52 +0000

    Branched from "svn+ssh://mouginot@svn.in2p3.fr/class/source/trunk".
    git-svn-id: svn+ssh://svn.in2p3.fr/class@657 0e7d625b-0364-4367-a6be-d5be4a48d228

---
+++ source/branches/CLASSV4.1/include/CLASSFacility.hxx
@@ -0,0 +1,214 @@
+
+#ifndef _CLASSFACILITY_
+#define _CLASSFACILITY_
+
+
+/*!
+ \file
+ \brief Header file for CLASSFacility class.
+
+ */
+
+#include <string>
+#include <fstream>
+
+#include "CLASSObject.hxx"
+#include "IsotopicVector.hxx"
+
+#include "TNamed.h"
+
+using namespace std;
+
+class Scenario;
+//-----------------------------------------------------------------------------//
+//! Defines the common properties of all facilities
+
+/*!
+ Define a CLASS Facility.
+ The aim of these class is to gather all the commom properties of the facilities.
+
+
+ @author BaM
+ @version 2.0
+ */
+//________________________________________________________________________
+
+
+
+
+class CLASSFacility : public CLASSObject
+{
+public :
+	/*!
+	 \name Constructor/Desctructor
+	 */
+	//@{
+	
+	//{
+	/// Normal Constructor.
+	/*!
+	Make a new Facility
+	\param type identification type of the facility :
+	\li 4 Reactor,
+	\li 8 Pool,
+	\li 16 FabricationPlant.
+	 */
+	
+	CLASSFacility(int type = 0);
+	//}
+
+	//{
+	/// Special Constructor.
+	/*!
+	 Make a new Facility
+	 \param log : used for the log.
+	 \param type identification type of the facility :
+	 \li 4 Reactor,
+	 \li 8 Pool,
+	 \li 16 FabricationPlant.
+	 
+	 */
+	CLASSFacility(CLASSLogger* log, int type = 0);
+	//}
+	
+	//{
+	/// Special Constructor.
+	/*!
+	 Make a new Facility
+	 \param log : used for the log.
+	 \param cycletime duration of the cycle [s],
+	 \param type identification type of the facility :
+	 \li 4 Reactor,
+	 \li 8 Pool,
+	 \li 16 FabricationPlant.
+	 
+	 */
+	CLASSFacility(CLASSLogger* log, cSecond cycletime, int type = 0);
+	//}
+	
+	//{
+	/// Special Constructor.
+	/*!
+	 Make a new Facility
+	 \param log : used for the log.
+	 \param creationtime creation date of the Facility [s],
+	 \param lifetime operating duration [s],
+	 \param type identification type of the facility :
+	 \li 4 Reactor,
+	 \li 8 Pool,
+	 \li 16 FabricationPlant.
+	 
+	 */
+	CLASSFacility(CLASSLogger* log, cSecond creationtime, cSecond lifetime, int type = 0);
+	//}
+	
+	//{
+	/// Special Constructor.
+	/*!
+	 Make a new Facility
+	 \param log : used for the log.
+	 \param creationtime creation date of the Facility [s],
+	 \param lifetime operating duration [s],
+	 \param cycletime duration of the cycle [s],
+	 \param type identification type of the facility :
+	 \li 4 Reactor,
+	 \li 8 Pool,
+	 \li 16 FabricationPlant.
+	 
+	 */
+	CLASSFacility(CLASSLogger* log, cSecond startingtime, cSecond lifetime, cSecond cycletime, int type = 0);
+	//}
+	
+	//********* Get Method *********//
+	/*!
+	 \name Get Function
+	 */
+	//@{
+
+	int 		GetId()			const	{ return fId; }			//!< Return the Facility Parc'Is
+	IsotopicVector 	GetInsideIV()		const	{ return fInsideIV; } 		//!< Return the IV contained in the Facility
+
+	int		GetFacilityType()	const	{ return fFacilityType; }	//!< Return the Facility Type id
+
+	cSecond		GetInternalTime()	const	{ return fInternalTime; }	//!< Return Creation Time
+
+	cSecond		GetCycleTime()		const	{ return fCycleTime; } 		//!< Return the cycle time of the Facility
+	cSecond 	GetCreationTime()	const	{ return fCreationTime; }	//!< Return the creation time of the Facility
+	cSecond 	GetLifeTime()		const	{ return fLifeTime; }		//!< Return the life time of the Facility
+	Scenario*	GetParc()			{ return fParc; }		//!< return the pointer to the Park
+
+
+	IsotopicVector GetCumulativeIVIn() { return fCumulativeIVIn;}			//!< return the cumulative sum of all incoming IV
+	IsotopicVector GetCumulativeIVOut() { return fCumulativeIVOut;}			//!< return the cumulative sum of all outcoming IV
+	//@}
+	
+		//********* Set Method *********//
+	/*!
+	 \name Set Function
+	 */
+	//@{
+	void	SetId(int id)			{ fId = id; }				//!< Set The Facility Parc'Id
+	void	SetParc(Scenario* parc)		{ fParc = parc; }			//!< Set the Pointer to the Parc
+	void	SetFacilityType(int type)	{ fFacilityType = type; }		//!< Set the facility type :
+											/// \li 2 reactor Studown
+											/// \li 4 start/End of reactor cycle,
+											/// \li 8 end of Cooling,
+											/// \li 16 fuel Fabrication
+	using CLASSObject::SetName;
+	using CLASSObject::GetName;
+
+
+	void SetInsideIV(IsotopicVector isotopicvector)	{ fInsideIV = isotopicvector; }	//!< Set the IV inside the Facility
+	void SetCreationTime(double creationtime)	{ fCreationTime = (cSecond)creationtime;} //!< Set the creation Time
+	void SetLifeTime(double lifetime)		{ fLifeTime = (cSecond)lifetime; }	//!< Set the life time of the facility
+	virtual void SetCycleTime(double cycletime)	{ fCycleTime = (cSecond)cycletime; }	//!< Set the cycle time (Cycle of the loading Plan)
+	void SetInCycleTime(double incycletime)		{ fInCycleTime = (cSecond)incycletime; fIsStarted = true; }	//!< Set the cycle time (Cycle of the loading Plan)
+	void SetInternalTime(double internaltime)	{ fInternalTime = (cSecond)internaltime; }	//!< Set the cycle time (Cycle of the loading Plan)
+
+	//@}
+
+
+	/*!
+	 \name Evolution Method
+	 */
+	//@{
+	virtual void ApplyZAIThreshold(int z = 90);						//!< Put all nuclei below the threshold in -2 -2 -2 ZAI...
+	void AddCumulativeIVIn(IsotopicVector IV) { fCumulativeIVIn += IV;}		//!< Add the Input IsotopicVector in the cumulative IV IN
+	void AddCumulativeIVOut(IsotopicVector IV) { fCumulativeIVOut += IV;}		//!< Add the Input IsotopicVector in the cumulative IV OUT
+	virtual void Evolution(cSecond t)	= 0;	//!< Performs the Evolution to time t
+	virtual void Dump()			{ }	//!< Write Modification (IV In/Out, filling the TF...)
+		
+	//@}
+protected :
+	bool		fIsStarted;		///< True if Running, False Otherwise
+	bool		fIsShutDown;		///< True if the facility is stoped, False Otherwise
+	bool		fIsAtEndOfCycle;	///< True if Reaching the end of a Facility cycle
+
+		
+	cSecond		fInternalTime;		///< Internal Clock [s]
+	cSecond		fInCycleTime;		///< Time spent since the beginning of the last Cycle [s]
+	cSecond		fCycleTime;		///< Cycle duration Time [s]
+
+	IsotopicVector	fInsideIV;		///< All IV in the Facility (fuel for reactor, total for all others...)
+	IsotopicVector	fCumulativeIVIn;	///< All IV in the Facility (fuel for reactor, total for all others...)
+	IsotopicVector	fCumulativeIVOut;	///< All IV in the Facility (fuel for reactor, total for all others...)
+
+		//********* Internal Parameter *********//
+private :
+	int		fId;			//!< Identity of the Facility inside the Scenario
+	int		fFacilityType;		///< Type of facility :
+						/// \li 4 reactor,
+						/// \li 8 Pool,
+						/// \li 16 FabricationPlant.
+
+
+	Scenario*	fParc;			//!< Pointer to the main Scenario
+
+	cSecond		fCreationTime;		///< CLASS Universal Time of Creation [s]
+	cSecond		fLifeTime;		///< Time of life Of the Reactor (operating's duration) [s]
+
+	ClassDef(CLASSFacility,1);
+};
+
+#endif
+
---
+++ source/branches/CLASSV4.1/src/CLASSNucleiFiliation.cxx
@@ -0,0 +1,322 @@
+#include "CLASSNucleiFiliation.hxx"
+#include "ZAI.hxx"
+#include "IsotopicVector.hxx"
+
+#include <map>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+//const string DEFAULTDATABASE = "DecayBase.dat";
+//________________________________________________________________________
+//
+//		CLASSNucleiFiliation
+//
+//
+//
+//
+//________________________________________________________________________
+//____________________________InClass Operator____________________________
+//________________________________________________________________________
+
+CLASSNucleiFiliation::CLASSNucleiFiliation():CLASSObject()
+{
+}
+CLASSNucleiFiliation::CLASSNucleiFiliation(CLASSLogger* log):CLASSObject(log)
+{
+}
+
+CLASSNucleiFiliation::CLASSNucleiFiliation(const CLASSNucleiFiliation& CNF):CLASSObject()
+{
+	fNucleiFiliation = CNF.GetNucleiFIliation();
+}
+//________________________________________________________________________
+CLASSNucleiFiliation::~CLASSNucleiFiliation()
+{
+	fNucleiFiliation.clear() ;
+}
+//________________________________________________________________________
+void CLASSNucleiFiliation::Add( ZAI Mother, IsotopicVector Daughter )
+{
+	DBGL
+	
+	pair< map<ZAI, IsotopicVector>::iterator, bool> IResult;
+	
+	
+	IResult = fNucleiFiliation.insert( pair<ZAI, IsotopicVector> ( Mother, Daughter ) );
+	
+	if( !IResult.second)
+		(*IResult.first).second += Daughter ;
+	
+	
+	DBGL
+}
+
+
+//________________________________________________________________________
+IsotopicVector CLASSNucleiFiliation::GetFiliation(ZAI Mother)
+{
+	DBGV(Mother.Z()<<" "<<Mother.A()<<" "<<Mother.I());
+	map<ZAI, IsotopicVector>::iterator it_Filiation;
+	
+	it_Filiation = fNucleiFiliation.find(Mother);	// search for the ZAI in the map
+
+	DBGL
+	if(it_Filiation != fNucleiFiliation.end())	// test if it is present in the map
+		return it_Filiation->second;
+	else
+		return ZAI(-1,-1,-1)*1;		// return -1 -1 _1 ZAI if unknown nuclei....
+	
+}
+
+//________________________________________________________________________
+void CLASSNucleiFiliation::FiliationCleanUp(map<ZAI, int> GoodNuclei, CLASSNucleiFiliation CuttedNuclei)
+{
+	DBGL
+	map<ZAI, IsotopicVector>::iterator it_Filiation;
+	for(it_Filiation = fNucleiFiliation.begin(); it_Filiation != fNucleiFiliation.end(); it_Filiation++) // loop on the filiation map (on the Mother ZAI)
+	{
+		vector<ZAI> DautherList = it_Filiation->second.GetZAIList();		// recover for each mother ZAI, the list of its daughter ZAI
+		
+		for (int i = 0; i < (int)DautherList.size(); i++)			// Loop on the Daughter ZAI
+		{
+			if(GoodNuclei.find(DautherList[i]) == GoodNuclei.end() ) // if the ZAI is not in a dealed nuclei (cutted or unknown)
+			{
+				double Daughter_BR = it_Filiation->second.GetQuantity(DautherList[i]);	// Get the quantity of the ZAI
+				it_Filiation->second -= Daughter_BR * DautherList[i];			// Remove it from the daughter list
+				
+				
+				IsotopicVector FastDecayChain = CuttedNuclei.GetFiliation(DautherList[i]); // Get the fast decay chain of it
+				
+				if(FastDecayChain.GetQuantity(-1, -1, -1) == 0) // Check if the FastDecayChain is known
+					it_Filiation->second += Daughter_BR * FastDecayChain; // Add the FastDecayCHain & apply the BR for the cutted Daughter
+				else
+				{
+					
+					ZAI Mother = DautherList[i];
+					while (FastDecayChain.GetQuantity(-1, -1, -1) != 0 && GoodNuclei.find(Mother) == GoodNuclei.end())
+					{
+						Mother = GetArtificialDecay(Mother);			// Do an Artifial decay on the nuclei
+						FastDecayChain = CuttedNuclei.GetFiliation(Mother); // Get the fast decay chain of it
+					}
+					
+					if(GoodNuclei.find(Mother) != GoodNuclei.end())
+						it_Filiation->second += Mother * Daughter_BR;
+					
+					else if ( FastDecayChain.GetQuantity(-1, -1, -1) == 0)
+						it_Filiation->second += FastDecayChain * Daughter_BR;
+					
+					else
+					{
+						ERROR << "Problem in Articial Decay!! check it!!" << endl;
+						exit(1);
+					}
+
+				}
+			}
+		}
+		
+	}
+	DBGL
+}
+//________________________________________________________________________
+
+ZAI CLASSNucleiFiliation::GetArtificialDecay(ZAI Mother)
+{
+	DBGL
+
+	int A = Mother.A();
+	int Z = Mother.Z();
+	int I = Mother.I();
+	
+	if(I!=0)
+		return ZAI(Z,A,I-1);
+	else
+	{
+		//Coef Ac & As of Bette & Weisacker are approximativ but enough precise for this application....
+		double Ac = 0.695;
+		double As = 23.2;
+		
+		double ZTh = A/2 * ( 1 )/ ( 1 + Ac / (4*As) * pow(A,2/3) );  // Stable Z from isobarn calculation using Bette & Weisacker formula.
+	
+		
+		if( Z > ZTh )		// Then Beta+
+			return ZAI(Z-1,A,I);
+		else			// Then Beta-
+			return ZAI(Z+1,A,I);
+		
+	}
+	
+	
+	DBGL
+}
+
+
+//________________________________________________________________________
+void CLASSNucleiFiliation::SelfFiliationCleanUp(map<ZAI, int> GoodNuclei)
+{
+	DBGL
+
+	bool Cleaned = false;
+	
+	while (!Cleaned)	// loop until all the map is cleaned (all cut have been done)
+	{
+		int count = 0;
+		map<ZAI, IsotopicVector> CopyfNucleiFiliation = fNucleiFiliation;
+		map<ZAI, IsotopicVector>::iterator it_Filiation;
+		for(it_Filiation = fNucleiFiliation.begin(); it_Filiation != fNucleiFiliation.end(); it_Filiation++) // Loop on the mother ZAI
+		{
+			vector<ZAI> DautherList = it_Filiation->second.GetZAIList(); // Get the list of daughter ZAI
+			
+			for (int i = 0; i < (int)DautherList.size(); i++)		//Loop on daughter
+			{
+				if(GoodNuclei.find(DautherList[i]) == GoodNuclei.end() ) // if the ZAI is not in a dealed nuclei (cutted or unknown)
+				{	count++;
+					map<ZAI, IsotopicVector>::iterator it_FiliationCopy = CopyfNucleiFiliation.find(it_Filiation->first)  ;
+
+					double Daughter_BR = it_FiliationCopy->second.GetQuantity(DautherList[i]);	// Get the quantity of the ZAI
+					it_FiliationCopy->second -= Daughter_BR * DautherList[i];			// Remove it from the daughter list					
+					IsotopicVector FastDecayChain = (*this).GetFiliation(DautherList[i]); // Get the fast decay chain of it
+
+					if(FastDecayChain.GetQuantity(-1, -1, -1) == 0) // Check if the FastDecayChain is known
+						it_FiliationCopy->second += Daughter_BR * FastDecayChain; // Add the FastDecayCHain & apply the BR for the cutted Daughter
+					else
+					{					
+						ZAI Mother = DautherList[i];
+						while (FastDecayChain.GetQuantity(-1, -1, -1) != 0 && GoodNuclei.find(Mother) == GoodNuclei.end())
+						{
+							Mother = GetArtificialDecay(Mother);			// Do an Artifial decay on the nuclei
+							FastDecayChain = (*this).GetFiliation(Mother); // Get the fast decay chain of it
+						}
+
+						if(GoodNuclei.find(Mother) != GoodNuclei.end())
+							it_FiliationCopy->second += Mother * Daughter_BR;
+						
+						else if ( FastDecayChain.GetQuantity(-1, -1, -1) == 0)
+							it_FiliationCopy->second += FastDecayChain * Daughter_BR;
+						
+						else
+						{
+							ERROR << "Problem in Articial Decay!! check it!!" << endl;
+							exit(1);
+						}
+
+					}
+				}
+			}
+			
+		}
+		fNucleiFiliation = CopyfNucleiFiliation;
+			Cleaned = true;
+		for(it_Filiation = fNucleiFiliation.begin(); it_Filiation != fNucleiFiliation.end(); it_Filiation++) // Loop on the mother ZAI
+		{
+			vector<ZAI> DautherList = it_Filiation->second.GetZAIList(); // Get the list of daughter ZAI
+			
+			for (int i = 0; i < (int)DautherList.size(); i++)		//Loop on daughter
+				if(GoodNuclei.find(DautherList[i]) == GoodNuclei.end() ) // if the ZAI is not in a dealed nuclei (cutted or unknown)
+					Cleaned = false;
+		}
+
+	}
+	
+	NormalizeBranchingRatio();
+	DBGL
+}
+
+
+
+//________________________________________________________________________
+void CLASSNucleiFiliation::NormalizeBranchingRatio(double Value)
+{
+	DBGL
+	map<ZAI, IsotopicVector>::iterator it_Filiation;
+	for(it_Filiation = fNucleiFiliation.begin(); it_Filiation != fNucleiFiliation.end(); it_Filiation++)
+	{
+		it_Filiation->second *= Value/it_Filiation->second.GetSumOfAll();
+	}
+	
+	DBGL
+}
+
+
+//________________________________________________________________________
+void CLASSNucleiFiliation::NormalizeBranchingRatio(ZAI Mother, double Value)
+{
+	DBGL
+	map<ZAI, IsotopicVector>::iterator it_Filiation = fNucleiFiliation.find(Mother);
+	
+	if( it_Filiation != fNucleiFiliation.end())
+		it_Filiation->second *= Value/it_Filiation->second.GetSumOfAll();
+	else
+		WARNING << "Trying to normaliza a Branching Ratio for a Mother wich are not present in the Filiatiuon List...." << endl;
+	
+	DBGL
+}
+
+
+//________________________________________________________________________
+
+vector<ZAI> CLASSNucleiFiliation::GetZAIList() const
+{
+	
+	map<ZAI ,IsotopicVector > IsotopicQuantity = GetNucleiFIliation();
+	map<ZAI ,IsotopicVector >::iterator it;
+	vector<ZAI> zailist;
+	for( it = IsotopicQuantity.begin(); it != IsotopicQuantity.end(); it++)
+		zailist.push_back( (*it).first );
+	
+	return zailist;
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
