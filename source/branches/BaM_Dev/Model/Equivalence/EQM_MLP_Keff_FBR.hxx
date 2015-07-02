8955736cac819c62c72696a8abec22792996c95e
mouginot <mouginot@0e7d625b-0364-4367-a6be-d5be4a48d228>
2015-07-02 10:14:03 +0000

    Commit For BaL :
    Adding 2 equivalence Models
    
    git-svn-id: svn+ssh://svn.in2p3.fr/class@683 0e7d625b-0364-4367-a6be-d5be4a48d228

---
+++ source/branches/BaM_Dev/Model/Equivalence/EQM_MLP_Keff_FBR.hxx
@@ -0,0 +1,174 @@
+#ifndef _EQM_MLP_Keff_FBR_HXX
+#define _EQM_MLP_Keff_FBR_HXX
+
+#include "EquivalenceModel.hxx"
+#include "TTree.h"
+#include "TGraph.h"
+
+/*!
+ \file
+ \brief Header file for EQM_MLP_Keff_FBR class.
+
+
+ @author BLG
+ @version 1.0
+ */
+
+
+using namespace std;
+
+//-----------------------------------------------------------------------------//
+//! Defines an EquivalenceModel based on neural network to predict @f$k_{eff}@f$
+/*!
+The aim of these class is to constuct a fuel from an equivalence model
+based on a  Multi layer perceptron (MLP). 
+This MLP aims to predict either :
+
+Method 1 :
+
+The @f$k_{\infty}(t)@f$ of a FBR-MOX from a given fresh fuel composition.
+With this MLP prediction and a given number of batch (for the loading plan) an 
+average @f$<k_{\infty}(t)>@f$ is calculated according :
+@f$<k_{\infty}>^{batch}(t) = \frac{1}{N}\sum_{i}^{N} k_{\infty}(t + \frac{iT}{N} )@f$
+The Fissile content has to verify this condition :
+@f$ k_{\infty Max} \geq <k_{\infty}>^{batch}(T/N) \geq k_{\infty Min} @f$
+Where @f$ k_{\infty Max}@f$ and @f$k_{\infty Min}@f$ are arguments of the constructor.
+
+Method 2 :
+
+Another constructor allows to perform the following: 
+The Pu content is set such as it has to verify
+ @f$ k(WantedTime) = k_{target}@f$  with @f$k_{target}@f$ is often close to 1.0 
+ but can be set by user. The wanted time is often either 
+ the begining of cycle or end of cycle. WantedTime can't be set by user since it is
+ contain in the .xml file. Indeed this method suppose you have trained your MLP to predict 
+ the keffective either at BOC or EOC (or any other time)
+
+ \warning 
+ With method 1, it is not guaranted that there is a solution for Pu content verifying : 
+ @f$<k_{\infty}>^{batch}(t) = \frac{1}{N}\sum_{i}^{N}k_{\infty}(t+\frac{iT}{N} )@f$
+
+ @author BLG
+ @version 1.0
+ */
+//________________________________________________________________________
+
+
+class EQM_MLP_Keff_FBR : public EquivalenceModel
+{
+	public :
+	/*!
+	 \name Constructor
+	 */
+	//@{
+
+	//{
+	///  Create a EQM_MLP_Keff_FBR using @f$k_{\infty}@f$ average over batch
+ 	/// (see class desctiption)
+ 	/*!
+	 Create a EQM_MLP_Keff_FBR using @f$k_{\infty}@f$ average over batch
+	 \param  TMVAWeightPath0:  Path to the .xml file containing neural network informations for prediction of keff(t)
+	 \param  NumOfBatch : Number of batch for the loading plan (often 5 for FBR-Na)
+	 \param  KeffMin : Lower average @f$<k_{\infty}>@f$ value 
+	 \param  KeffMax : Upper average @f$<k_{\infty}>@f$ value 
+	 \param  InformationFile : Total path to the file containing time steps, fissile and ferile list (ante and post fabrication time cooling). Default is the same total path as TMVAWeightPath but extension is replaced by .nfo (see manual for format)
+	 */
+	EQM_MLP_Keff_FBR(string TMVAWeightPath,  int NumOfBatch , double LowerKeffective, double UpperKeffective, string InformationFile="");
+	//}
+
+	//{
+	///  Create a EQM_MLP_Keff_FBR using @f$k_{\infty}@f$ average over batch 
+ 	/// (see class desctiption)
+ 	/*!
+	 Create a EQM_MLP_Keff_FBR using @f$k_{\infty}@f$ average over batch
+	 \param  log:  CLASSLogger object to handle log messages
+	 \param  TMVAWeightPath0:  Path to the .xml file containing neural network informations for prediction of keff(t)
+	 \param  NumOfBatch : Number of batch for the loading plan (often 5 for FBR-Na)
+	 \param  KeffMin : Lower average @f$<k_{\infty}>@f$ value 
+	 \param  KeffMax : Upper average @f$<k_{\infty}>@f$ value 
+	 \param  InformationFile : Total path to the file containing time steps, fissile and ferile list (ante and post fabrication time cooling). Default is the same total path as TMVAWeightPath but extension is replaced by .nfo (see manual for format)
+	 */
+	EQM_MLP_Keff_FBR(CLASSLogger* log, string TMVAWeightPath,  int NumOfBatch , double LowerKeffective, double UpperKeffective, string InformationFile="");
+	//}
+
+	//{
+	///  Create a EQM_MLP_Keff_FBR using Keffective at a given time
+ 	/// (see class desctiption)
+	 /*!
+	 Create a EQM_MLP_Keff_FBR 
+	 \param  TMVAWeightPath :  Path to the .xml file containing neural network informations for prediction of keff(t=tfixed)
+	 \param  keff_target : Wanted  @f$k_{eff}@f$ (see detailed description)
+	 \param  InformationFile : Total path to the file containing time steps, fissile and ferile list (ante and post fabrication time cooling). Default is the same total path as TMVAWeightPath but extension is replaced by .nfo (see manual for format)
+	 */
+	EQM_MLP_Keff_FBR(string TMVAWeightPath, double keff_target = 1.00, string InformationFile="");
+	//}
+
+	//{
+	///  Create a EQM_MLP_Keff_FBR using Keffective at a given time
+ 	/// (see class desctiption)
+	 /*!
+	 Create a EQM_MLP_Keff_FBR 
+	 \param  log:  CLASSLogger object to handle log messages
+	 \param  TMVAWeightPath :  Path to the .xml file containing neural network informations for prediction of keff(t=tfixed)
+	 \param  keff_target : Wanted  @f$k_{eff}@f$ (see detailed description)
+	 \param  InformationFile : Total path to the file containing time steps, fissile and ferile list (ante and post fabrication time cooling). Default is the same total path as TMVAWeightPath but extension is replaced by .nfo (see manual for format)
+	 */
+	EQM_MLP_Keff_FBR(CLASSLogger* log, string TMVAWeightPath, double keff_target = 1.00, string InformationFile="");
+	//}
+
+	//@}
+	
+	//{
+	/// Return the molar fissile fraction according fissile & ferile content using @f$<k_{\infty}>(t)@f$ prediction
+	/*!
+	 \param Fissil : The composition of the fissile matter
+	 \param Fertil : The composition of the Fertil matter
+	 \param BurnUp : Maximum achievable burn up envisaged
+	 */
+	virtual double GetFissileMolarFraction(IsotopicVector Fissil,IsotopicVector Fertil,double BurnUp=0);
+	//}
+	
+	/*!
+	 \name Get/Set methods
+	 */
+	//@{
+	void 	SetPCMprecision(double pcm){fPCMprecision = pcm;}	//!< Set the precision on <k> prediction [pcm]. Neural network predictor constructors
+	double 	GetPCMprecision(){return fPCMprecision/1e5;}		//!< Get the precision on <k> prediction []. Neural network predictor constructors
+
+	//@}
+	TTree* 	CreateTMVAInputTree(IsotopicVector FreshFuel, double ThisTime);//!<Create input tmva tree to be read by ExecuteTMVA
+	double 	ExecuteTMVA(TTree* theTree, bool IsTimeDependant);//!<Execute the MLP according to the input tree created by CreateTMVAInputTree
+
+
+	private :
+	void   	GetModelInformation();//!<Read the fMLPInformationFile and fill containers and variables
+	double 	GetKeffAtFixedTime(IsotopicVector FreshFuel){return ExecuteTMVA( CreateTMVAInputTree(FreshFuel,-1), false );} //!<time independant since the MLP is trained for 1 time
+	TGraph* BuildKeffGraph(IsotopicVector FreshFuel);
+	TGraph* BuildAverageKeffGraph(TGraph* GRAPH_KEFF);
+	double 	GetKeffAt(TGraph* GRAPH_KEFF, int Step);
+	double  GetFissileContent_keffAveragePredictor(IsotopicVector Fissile, IsotopicVector Fertile);
+	double  GetFissileMolarFraction_keffPredictor(IsotopicVector Fissile,IsotopicVector Fertile);
+
+
+
+	int 	fNumberOfBatch;		//!< The number of batches for the loading plan
+	double 	fKThreshold;		//!< The @f$k_{Threshold}@f$
+	double 	fSpecificPower; 	//!< The specific power in W/gHM (HM: heavy Metal)
+	double  fMaximalContent;	//!< The approx. maximum fissile content reachable by the MLP model
+	double 	fPCMprecision;		//!< precision on <k> prediction [pcm]
+	bool	fIsAverageKeff;		//!< True if using the first contructor (fissile content prediction with average keff (over batches))
+	double  fKmin;				//!< Lower edge of kedd Used by second constructor (fissile content prediction using keff at BOC (or other time)
+	double  fKmax;				//!< Upper edge of kedd Used by second constructor (fissile content prediction using keff at BOC (or other time)
+	double 	fTargetKeff;		//!< Use for Varying Fissile content to reach fTargetKeff at time used in the MLP Training
+
+	vector<double> fMLP_Time;	//!< Time (in seconds) when the MLP(t)=keff(t) has been trained.
+
+			
+	map<ZAI,string> fMapOfTMVAVariableNames;//!<  List of TMVA input variable names (read from fMLPInformationFile ) , name depends on the training step
+
+	string fTMVAWeightPath;					//!<The weight needed by TMVA to construct and execute the multilayer perceptron
+	string fMLPInformationFile;				//!<The path to the informations necessary to execute the MLP
+};
+
+#endif
+
