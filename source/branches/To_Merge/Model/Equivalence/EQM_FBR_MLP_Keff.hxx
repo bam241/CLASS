852a77ac56f097b10498aef82f394d1c2114f5ef
Baptiste LENIAU <baptiste.leniau@subatech.in2p3.fr>
2016-04-18 12:58:25 +0000

    Branched from "svn+ssh://leniau@svn.in2p3.fr/class/source/trunk".
    git-svn-id: svn+ssh://svn.in2p3.fr/class@803 0e7d625b-0364-4367-a6be-d5be4a48d228

---
+++ source/branches/To_Merge/Model/Equivalence/EQM_FBR_MLP_Keff.hxx
@@ -0,0 +1,199 @@
+#ifndef _EQM_FBR_MLP_Keff_HXX
+#define _EQM_FBR_MLP_Keff_HXX
+
+#include "EquivalenceModel.hxx"
+#include "TTree.h"
+#include "TGraph.h"
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+class EQM_FBR_MLP_Keff;
+#ifndef __CINT__
+typedef void (EQM_FBR_MLP_Keff::*FBR_MLP_Keff_DMthPtr)( const string & ) ;
+#endif
+
+//-----------------------------------------------------------------------------//
+//! Defines an EquivalenceModel based on neural network to predict @f$k_{eff}@f$.
+
+/*!
+The aim of these class is to constuct a fuel from an equivalence model
+based on a  Multi layer perceptron (MLP).
+This MLP aims to predict the Pu content such as it has to verify
+ @f$ k(WantedTime) = k_{target}@f$  with @f$k_{target}@f$ is often close to 1.0 
+ but can be set by user. The wanted time is often either 
+ the begining of cycle or end of cycle. WantedTime can't be set by user since it is
+ contain in the .xml file. Indeed this method suppose you have trained your MLP to predict 
+ the @f$k_{eff}@f$ either at BOC or EOC (or any other time)
+
+ @author BLG
+ @author BaM
+ @version 1.0
+ */
+//________________________________________________________________________
+
+class EQM_FBR_MLP_Keff : public EquivalenceModel
+{
+	public:
+	/*!
+	 \name Constructor
+	 */
+	//@{
+
+
+
+	//{
+	///  Create a EQM_FBR_MLP_Keff using Keffective at a given time
+ 	/// (see class desctiption)
+	 /*!
+	 Create a EQM_FBR_MLP_Keff 
+	 \param  TMVAWeightPath :  Path to the .xml file containing neural network informations for prediction of keff(t = tfixed)
+	 \param  keff_target : Wanted  @f$k_{eff}@f$ (see detailed description)
+	 \param  InformationFile : Total path to the file containing time steps, fissile and ferile list (ante and post fabrication time cooling). Default is the same total path as TMVAWeightPath but extension is replaced by .nfo (see manual for format)
+	 */
+	EQM_FBR_MLP_Keff(string TMVAWeightPath, double keff_target = 1.00, string InformationFile = "");
+	//}
+
+	//{
+	///  Create a EQM_FBR_MLP_Keff using Keffective at a given time
+ 	/// (see class desctiption)
+	 /*!
+	 Create a EQM_FBR_MLP_Keff 
+	 \param  log:  CLASSLogger object to handle log messages
+	 \param  TMVAWeightPath :  Path to the .xml file containing neural network informations for prediction of keff(t = tfixed)
+	 \param  keff_target : Wanted  @f$k_{eff}@f$ (see detailed description)
+	 \param  InformationFile : Total path to the file containing time steps, fissile and ferile list (ante and post fabrication time cooling). Default is the same total path as TMVAWeightPath but extension is replaced by .nfo (see manual for format)
+	 */
+	EQM_FBR_MLP_Keff(CLASSLogger* log, string TMVAWeightPath, double keff_target = 1.00, string InformationFile = "");
+	//}
+
+	//@}
+	
+	//{
+	/// Return the molar fissile fraction according fissile & ferile content using @f$<k_{\infty}>(t)@f$ prediction
+	/*!
+	 \param Fissil : The composition of the fissile matter
+	 \param Fertil : The composition of the Fertil matter
+	 \param BurnUp : Maximum achievable burn up envisaged
+	 */
+	virtual double GetFissileMolarFraction(IsotopicVector Fissil,IsotopicVector Fertil,double BurnUp = 0);
+	//}
+	
+	/*!
+	 \name Get/Set methods
+	 */
+	//@{
+	void 	SetPCMprecision(double pcm){fPCMprecision = pcm;}	//!< Set the precision on @f$\langle k \rangle@f$ prediction [pcm]. Neural network predictor constructors
+	double 	GetPCMprecision(){return fPCMprecision/1e5;}		//!< Get the precision on @f$\langle k \rangle@f$ prediction []. Neural network predictor constructors
+
+	//@}
+	
+	
+	
+	
+	
+	/*!
+	 \name TMVA related  methods
+	 */
+	//@{
+	TTree* 	CreateTMVAInputTree(IsotopicVector FreshFuel, double ThisTime);//!<Create input tmva tree to be read by ExecuteTMVA
+	
+	double 	ExecuteTMVA(TTree* theTree, bool IsTimeDependant);//!<Execute the MLP according to the input tree created by CreateTMVAInputTree
+	//@}
+	
+	
+	
+	/*!
+	 \name Reading NFO related Method
+	 */
+	//@{
+	
+	//{
+	/// LoadKeyword() : make the correspondance between keyword and reading method
+	void LoadKeyword();
+	//}
+	
+	//{
+	/// ReadTimeSteps : read the time step of the model
+	/*!
+	 \param line : line suppossed to contain the time step information starts with "k_timestep" keyword
+	 */
+	void ReadTimeSteps(const string &line);
+	//}
+	
+	
+	//{
+	/// ReadZAIName : read the zai name in the TMWA MLP model
+	/*!
+	 \param line : line suppossed to contain the ZAI name  starts with "k_zainame" keyword
+	 */
+	void ReadZAIName(const string &line);
+	//}
+	
+	//{
+	/// ReadMaxFisContent  : read a guessed (very overestimated) maximum fissile content (purpose : algorithm initialization)
+	/*!
+	 \param line : line suppossed to contain the ZAI name  starts with "k_maxfiscontent" keyword
+	 */
+	void ReadMaxFisContent(const string &line);
+	//}
+	
+	//{
+	/// ReadLine : read a line
+	/*!
+	 \param line : line to read
+	 */
+	void ReadLine(string line);
+	//}
+	
+	//@}
+	
+	
+	
+	private :
+
+	string fTMVAWeightPath;					//!<The weight needed by TMVA to construct and execute the multilayer perceptron
+
+#ifndef __CINT__
+	map<string, FBR_MLP_Keff_DMthPtr> fDKeyword;
+#endif
+
+
+	map<ZAI,string> fMapOfTMVAVariableNames;//!<  List of TMVA input variable names (read from fMLPInformationFile ) , name depends on the training step
+
+	vector<double> fMLP_Time;	//!< Time (in seconds) when the MLP(t) = keff(t) has been trained.
+	double  fMaximalContent;	//!< The approx. maximum fissile content reachable by the MLP model
+	
+
+	
+	int 	fNumberOfBatch;		//!< The number of batches for the loading plan
+	
+	double 	fKThreshold;		//!< The @f$k_{Threshold}@f$
+	double 	fPCMprecision;		//!< precision on @f$\langle k \rangle@f$ prediction [pcm]
+	
+	double 	fTargetKeff;		//!< Use for Varying Fissile content to reach fTargetKeff at time used in the MLP Training
+	
+
+	/*!
+	 \name keff prediction methods & keff averaging
+	 */
+	//@{
+
+		double 	GetKeffAtFixedTime(IsotopicVector FreshFuel){TTree* Input = CreateTMVAInputTree(FreshFuel,-1);  double Keff = ExecuteTMVA( Input, false ); delete Input; return Keff;} //!<time independant since the MLP is trained for 1 time
+
+		TGraph* BuildKeffGraph(IsotopicVector FreshFuel);
+		TGraph* BuildAverageKeffGraph(TGraph* GRAPH_KEFF);
+		double 	GetKeffAt(TGraph* GRAPH_KEFF, int Step);
+
+	//@}	
+
+
+	
+};
+
+#endif
+
---
+++ source/branches/To_Merge/Model/Irradiation/IM_Matrix.cxx
@@ -0,0 +1,301 @@
+//
+//  IM_Matrix.cxx
+//  CLASSSource
+//
+//  Created by BaM on 04/05/2014.
+//  Copyright (c) 2014 BaM. All rights reserved.
+//
+
+#include "IM_Matrix.hxx"
+
+#include "IsotopicVector.hxx"
+#include "CLASSConstante.hxx"
+#include "CLASSLogger.hxx"
+#include "StringLine.hxx"
+
+#include <TGraph.h>
+#include <TString.h>
+
+
+#include <sstream>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <cmath>
+
+
+
+using namespace std;
+
+
+//________________________________________________________________________
+IM_Matrix::IM_Matrix():IrradiationModel(new CLASSLogger("IM_Matrix.log"))
+{
+	fShorstestHalflife = 3600.*24*160.; //cut by default all nuclei with a shorter liftime than the Cm242 -> remain 33 actinides
+}
+
+
+IM_Matrix::IM_Matrix(CLASSLogger* log):IrradiationModel(log)
+{
+	fShorstestHalflife = 3600.*24*160.; //cut by default all nuclei with a shorter liftime than the Cm242 -> remain 33 actinides
+}
+
+
+
+
+//________________________________________________________________________
+/*			Evolution Calculation			*/
+//________________________________________________________________________
+EvolutionData IM_Matrix::GenerateEvolutionData(IsotopicVector isotopicvector, EvolutionData XSSet, double Power, double cycletime)
+{
+	DBGL
+	if(fFastDecay.size() ==  0)
+		NuclearDataInitialization();
+
+
+	string ReactorType;
+
+
+	vector< TMatrixT<double> > NMatrix ;//  TMatrixT<double>(decayindex.size(),1))
+	{	// Filling the t = 0 State;
+		map<ZAI, double > isotopicquantity = isotopicvector.GetIsotopicQuantity();
+		TMatrixT<double>  N_0Matrix = TMatrixT<double>( fReverseMatrixIndex.size(),1) ;
+		for(int i = 0; i < (int)fReverseMatrixIndex.size(); i++)
+			N_0Matrix[i] = 0;
+
+		map<ZAI, double >::iterator it ;
+		for(int i = 0; i < (int)fReverseMatrixIndex.size(); i++)
+			N_0Matrix[i] = 0;
+
+		for(it = isotopicquantity.begin(); it != isotopicquantity.end(); it++)
+		{
+			/// Need TO change with FP managment
+			map<ZAI, int >::iterator it2;
+
+			if( (*it).first.Z() < fZAIThreshold )
+				it2 = fMatrixIndex.find( ZAI(-2,-2,-2) );
+			else it2 = fMatrixIndex.find( (*it).first );
+
+			if(it2 ==  fMatrixIndex.end() )		//If not in index should be TMP, can't be fast decay for new Fuel !!!
+				it2 = fMatrixIndex.find( ZAI(-3,-3,-3) );
+			N_0Matrix[ (*it2).second ][0] = (*it).second ;
+
+
+		}
+
+		isotopicquantity.clear();
+
+		NMatrix.push_back(N_0Matrix);
+		N_0Matrix.Clear();
+
+	}
+
+
+	//-------------------------//
+	//--- Perform Evolution ---//
+	//-------------------------//
+	ReactorType = XSSet.GetReactorType();
+
+	double M_ref = XSSet.GetHeavyMetalMass();
+	double M = cZAIMass.GetMass(isotopicvector.GetActinidesComposition());
+	double Power_ref = XSSet.GetPower();
+
+	int NStep = XSSet.GetFissionXS().begin()->second->GetN();
+	double* DBTimeStep = XSSet.GetFissionXS().begin()->second->GetX();
+
+	int InsideStep = 10;
+
+	double timevector[NStep];
+	timevector[0] = 0;
+
+	double  Flux[NStep];
+
+	TMatrixT<double> FissionEnergy = TMatrixT<double>(fReverseMatrixIndex.size(),1);
+	for(int i = 0; i < (int)fReverseMatrixIndex.size(); i++)
+		FissionEnergy[i] = 0;
+
+	{
+		map< ZAI, int >::iterator it;
+		for(it = fMatrixIndex.begin(); it != fMatrixIndex.end(); it++)
+		{
+			map< ZAI, double >::iterator it2 = fFissionEnergy.find(it->first);
+			if(it2 ==  fFissionEnergy.end())
+			{
+				if(it->first.Z() > fZAIThreshold)
+					FissionEnergy[it->second][0] = 1.9679e6*it->first.A()-2.601e8; // //simple linear fit to known values ;extrapolation to unknown isotopes
+				else FissionEnergy[it->second][0] = 0;
+			}
+			else
+				FissionEnergy[it->second][0] = it2->second;
+
+		}
+	}
+
+	vector< TMatrixT<double> > FissionXSMatrix;	// Store The Fisison XS Matrix
+	vector< TMatrixT<double> > CaptureXSMatrix;	// Store The Capture XS Matrix
+	vector< TMatrixT<double> > n2nXSMatrix;		// Store The n2N XS Matrix
+	DBGL
+	for(int i = 0; i < NStep-1; i++)
+	{
+		double TStepMax = ( (DBTimeStep[i+1]-DBTimeStep[i] ) ) * Power_ref/M_ref / Power*M ;	// Get the next Time step
+
+
+		TMatrixT<double> BatemanMatrix = TMatrixT<double>(fReverseMatrixIndex.size(),fReverseMatrixIndex.size());
+		TMatrixT<double> BatemanReactionMatrix = TMatrixT<double>(fReverseMatrixIndex.size(),fReverseMatrixIndex.size());
+
+		TMatrixT<double> NEvolutionMatrix = TMatrixT<double>(fReverseMatrixIndex.size(),1);
+		NEvolutionMatrix = NMatrix.back();
+
+
+
+		FissionXSMatrix.push_back(GetFissionXsMatrix(XSSet, DBTimeStep[i]));	//Feel the fission reaction Matrix
+		CaptureXSMatrix.push_back(GetCaptureXsMatrix(XSSet, DBTimeStep[i]));	//Feel the capture reaction Matrix
+		n2nXSMatrix.push_back(Getn2nXsMatrix(XSSet, DBTimeStep[i]));		//Feel the (n,2n)  reaction Matrix
+
+		// ----------------   Evolution
+
+		BatemanReactionMatrix = FissionXSMatrix[i];
+		BatemanReactionMatrix += CaptureXSMatrix[i];
+		BatemanReactionMatrix += n2nXSMatrix[i];
+
+		for(int k = 0; k < InsideStep; k++)
+		{
+			double ESigmaN = 0;
+			for (int j = 0; j < (int)fReverseMatrixIndex.size() ; j++)
+				ESigmaN -=  FissionXSMatrix[i][j][j]*NEvolutionMatrix[j][0]*1.6e-19*FissionEnergy[j][0];
+			// Update Flux
+			double Flux_k = Power/ESigmaN;
+
+			if(k == 0)
+				Flux[i] = Flux_k;
+
+			BatemanMatrix = BatemanReactionMatrix;
+			BatemanMatrix *=  Flux_k;
+			BatemanMatrix += fDecayMatrix ;
+			BatemanMatrix *=  TStepMax/InsideStep ;
+
+
+			TMatrixT<double> IdMatrix = TMatrixT<double>(fReverseMatrixIndex.size(),fReverseMatrixIndex.size());
+			for(int j = 0; j < (int)fReverseMatrixIndex.size(); j++)
+				for(int k = 0; k < (int)fReverseMatrixIndex.size(); k++)
+				{
+					if(k ==  j)	IdMatrix[j][k] = 1;
+					else 		IdMatrix[j][k] = 0;
+				}
+
+
+			TMatrixT<double> BatemanMatrixDL = TMatrixT<double>(fReverseMatrixIndex.size(),fReverseMatrixIndex.size());   // Order 0 Term from the DL : Id
+			TMatrixT<double> BatemanMatrixDLTermN = TMatrixT<double>(fReverseMatrixIndex.size(),fReverseMatrixIndex.size());  // Addind it;
+
+			{
+				BatemanMatrix *=  TStepMax ;
+				BatemanMatrixDLTermN = IdMatrix;
+				BatemanMatrixDL = BatemanMatrixDLTermN;
+				int j = 1;
+				double NormN;
+
+				do
+				{
+					TMatrixT<double> BatemanMatrixDLTermtmp = TMatrixT<double>(fReverseMatrixIndex.size(),fReverseMatrixIndex.size());  // Adding it;
+					BatemanMatrixDLTermtmp = BatemanMatrixDLTermN;
+
+					BatemanMatrixDLTermN.Mult(BatemanMatrixDLTermtmp, BatemanMatrix );
+
+					BatemanMatrixDLTermN *=  1./j;
+					BatemanMatrixDL += BatemanMatrixDLTermN;
+
+					NormN = 0;
+					for(int m = 0; m < (int)fReverseMatrixIndex.size(); m++)
+						for(int n = 0; n < (int)fReverseMatrixIndex.size(); n++)
+							NormN += BatemanMatrixDLTermN[m][n]*BatemanMatrixDLTermN[m][n];
+					j++;
+
+				} while ( NormN != 0 );
+			}
+			NEvolutionMatrix = BatemanMatrixDL * NEvolutionMatrix ;
+		}
+		NMatrix.push_back(NEvolutionMatrix);
+
+
+		timevector[i+1] = timevector[i] + TStepMax;
+
+		BatemanMatrix.Clear();
+		BatemanReactionMatrix.Clear();
+		NEvolutionMatrix.Clear();
+
+
+	}
+	DBGL
+	FissionXSMatrix.push_back(GetFissionXsMatrix(XSSet, DBTimeStep[NStep-1])); //Feel the reaction Matrix
+	CaptureXSMatrix.push_back(GetCaptureXsMatrix(XSSet, DBTimeStep[NStep-1])); //Feel the reaction Matrix
+	n2nXSMatrix.push_back(Getn2nXsMatrix(XSSet, DBTimeStep[NStep-1])); //Feel the reaction Matrix
+
+
+	EvolutionData GeneratedDB = EvolutionData(GetLog());
+
+	double ESigmaN = 0;
+	for (int j = 0; j < (int)fReverseMatrixIndex.size() ; j++)
+		ESigmaN -=  FissionXSMatrix.back()[j][j]*NMatrix.back()[j][0]*1.6e-19*FissionEnergy[j][0];
+
+	Flux[NStep-1] = Power/ESigmaN;
+
+	GeneratedDB.SetFlux( new TGraph(NStep, timevector, Flux) );
+
+	for(int i = 0; i < (int)fReverseMatrixIndex.size(); i++)
+	{
+		double ZAIQuantity[NMatrix.size()];
+		double FissionXS[NStep];
+		double CaptureXS[NStep];
+		double n2nXS[NStep];
+		for(int j = 0; j < (int)NMatrix.size(); j++)
+			ZAIQuantity[j] = (NMatrix[j])[i][0];
+
+		for(int j = 0; j < NStep; j++)
+		{
+			FissionXS[j] = FissionXSMatrix[j][i][i];
+			CaptureXS[j] = CaptureXSMatrix[j][i][i];
+			n2nXS[j] = n2nXSMatrix[j][i][i];
+		}
+
+		GeneratedDB.NucleiInsert(pair<ZAI, TGraph*> (fReverseMatrixIndex[i], new TGraph(NMatrix.size(), timevector, ZAIQuantity)));
+/*		GeneratedDB.FissionXSInsert(pair<ZAI, TGraph*> (fReverseMatrixIndex[i], new TGraph(NStep, timevector, FissionXS)));
+		GeneratedDB.CaptureXSInsert(pair<ZAI, TGraph*> (fReverseMatrixIndex[i], new TGraph(NStep, timevector, CaptureXS)));
+		GeneratedDB.n2nXSInsert(pair<ZAI, TGraph*> (fReverseMatrixIndex[i], new TGraph(NStep, timevector, n2nXS)));
+*/	}
+
+	GeneratedDB.SetPower(Power );
+	GeneratedDB.SetHeavyMetalMass(M);
+	GeneratedDB.SetReactorType(ReactorType );
+	GeneratedDB.SetCycleTime(cycletime);
+
+	for (int i = 0; i < (int) FissionXSMatrix.size(); i++)
+	{
+		FissionXSMatrix[i].Clear();
+		CaptureXSMatrix[i].Clear();
+		n2nXSMatrix[i].Clear();
+	}
+	FissionXSMatrix.clear();
+	CaptureXSMatrix.clear();
+	n2nXSMatrix.clear();
+	DBGL
+	return GeneratedDB;
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
