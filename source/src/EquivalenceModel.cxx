a31bbf06636dcffc076ec5b1fb633ef841ab887d
Nicolas Thiolliere <nicolas.thiolliere@subatech.in2p3.fr>
2017-02-06 15:47:01 +0100

    EquivalenceModel constructor Completed

--- source/src/EquivalenceModel.cxx
+++ source/src/EquivalenceModel.cxx
@@ -27,71 +27,33 @@ EquivalenceModel::EquivalenceModel(string TMVAXMLFilePath, string TMVANFOFilePat
     fTMVAXMLFilePath = TMVAXMLFilePath;
     fTMVANFOFilePath = TMVANFOFilePath;
 
+    fDBRType = "";
+    fDBFType = "";
     fSpecificPower = 0;
     fMaximalBU = 0;
     fTargetParameter = "";
+    fTargetParameterStDev = 0;
+    fBuffer = "";
     fPredictorType = "";
     fOutput = "";
-    fBuffer = ""; 
-    fTargetParameterStDev = 0;
 
     LoadKeyword();  // Load Key words defineds in NFO file
     ReadNFO();      //Getting information from file NFO
 
-    //Check if k_targetparameter is defined in file NFO
-    if(fTargetParameter.empty())  
-    {
-        ERROR<<"Missing information for k_targetparameter in : "<<fTMVANFOFilePath<<endl;
-        exit(1);
-    }
-    //Check if fTargetParameterStDev is defined in file NFO
-    if(!fTargetParameterStDev)  
-    {
-        ERROR<<"Missing information for fTargetParameterStDev in : "<<fTMVANFOFilePath<<endl;
-        exit(1);
-    }
-    //Check if k_modelparameter is defined in file NFO
-    if(fModelParameter.empty())  
-    {
-        ERROR<<"Missing information for k_modelparameter in : "<<fTMVANFOFilePath<<endl;
-        exit(1);
-    }
-    //Check if k_predictortype is defined in file NFO
-    if(fPredictorType.empty())  
-    {
-        ERROR<<"Missing information for k_predictortype in : "<<fTMVANFOFilePath<<endl;
-        exit(1);
-    }
-    //Check if k_output is defined in file NFO
-    if(fOutput.empty())  
-    {
-        ERROR<<"Missing information for k_output in : "<<fTMVANFOFilePath<<endl;
-        exit(1);
-    }
-    //Check if k_buffer is defined in file NFO
-    if(fBuffer.empty())  
-    {
-        ERROR<<"Missing information for k_buffer in : "<<fTMVANFOFilePath<<endl;
-        exit(1);
-    }
-    //Check if k_massfractionmin and k_massfractionmax are defined in file NFO
-    if(fStreamListEqMMassFractionMin.empty() || fStreamListEqMMassFractionMax.empty())  
-    {
-        ERROR<<"Missing information for k_massfractionmin and/or k_massfractionmax in : "<<fTMVANFOFilePath<<endl;
-        exit(1);
-    }
-    //Check if k_zainame are defined in file NFO
-    if(fMapOfTMVAVariableNames.empty())  
-    {
-        ERROR<<"Missing information for k_zainame in : "<<fTMVANFOFilePath<<endl;
-        exit(1);
-    }
-    //Check if k_list are defined in file NFO
-    if(fStreamList.empty())  
-    {
-        ERROR<<"Missing information for k_list in : "<<fTMVANFOFilePath<<endl;
-        exit(1);
-    }
+    if(fZAILimits.empty()) {ERROR<<"Missing information for k_zail in : "<<fTMVANFOFilePath<<endl; exit(1);}
+    if(fDBRType.empty()) {ERROR<<"Missing information for k_reactor in : "<<fTMVANFOFilePath<<endl; exit(1);}
+    if(fDBFType.empty()) {ERROR<<"Missing information for k_fuel in : "<<fTMVANFOFilePath<<endl; exit(1);}
+    if(!fSpecificPower) {ERROR<<"Missing information for k_specpower in : "<<fTMVANFOFilePath<<endl; exit(1);}
+    if(!fMaximalBU) {ERROR<<"Missing information for k_maxburnup in : "<<fTMVANFOFilePath<<endl; exit(1);}
+    if(fStreamListEqMMassFractionMin.empty() || fStreamListEqMMassFractionMax.empty()) { ERROR<<"Missing information for k_massfractionmin and/or k_massfractionmax in : "<<fTMVANFOFilePath<<endl; exit(1);}
+    if(fStreamList.empty()) { ERROR<<"Missing information for k_list in : "<<fTMVANFOFilePath<<endl; exit(1); }
+    if(fMapOfTMVAVariableNames.empty()) { ERROR<<"Missing information for k_zainame in : "<<fTMVANFOFilePath<<endl; exit(1);}
+    if(fTargetParameter.empty()) { ERROR<<"Missing information for k_targetparameter in : "<<fTMVANFOFilePath<<endl; exit(1);}
+    if(!fTargetParameterStDev) { ERROR<<"Missing information for fTargetParameterStDev in : "<<fTMVANFOFilePath<<endl; exit(1);}
+    if(fModelParameter.empty()) { ERROR<<"Missing information for k_modelparameter in : "<<fTMVANFOFilePath<<endl; exit(1);}
+    if(fBuffer.empty()) { ERROR<<"Missing information for k_buffer in : "<<fTMVANFOFilePath<<endl; exit(1);}
+    if(fPredictorType.empty()) { ERROR<<"Missing information for k_predictortype in : "<<fTMVANFOFilePath<<endl; exit(1);}
+    if(fOutput.empty()) { ERROR<<"Missing information for k_output in : "<<fTMVANFOFilePath<<endl; exit(1);}
 
     INFO << "__An equivalence model has been define__" << endl;
     INFO << "\tThe TMVA weights file is :" << fTMVAXMLFilePath << endl;
@@ -109,71 +71,33 @@ EquivalenceModel::EquivalenceModel(CLASSLogger* log, string TMVAXMLFilePath, str
     fTMVAXMLFilePath = TMVAXMLFilePath;
     fTMVANFOFilePath = TMVANFOFilePath;
 
+    fDBRType = "";
+    fDBFType = "";
     fSpecificPower = 0;
     fMaximalBU = 0;
     fTargetParameter = "";
+    fTargetParameterStDev = 0;
+    fBuffer = "";
     fPredictorType = "";
     fOutput = "";
-    fBuffer = ""; 
-    fTargetParameterStDev = 0;
 
     LoadKeyword();  // Load Key words defineds in NFO file
     ReadNFO();      //Getting information from file NFO
 
-    //Check if k_targetparameter is defined in file NFO
-    if(fTargetParameter.empty())  
-    {
-        ERROR<<"Missing information for k_targetparameter in : "<<fTMVANFOFilePath<<endl;
-        exit(1);
-    }
-    //Check if fTargetParameterStDev is defined in file NFO
-    if(!fTargetParameterStDev)  
-    {
-        ERROR<<"Missing information for fTargetParameterStDev in : "<<fTMVANFOFilePath<<endl;
-        exit(1);
-    }
-    //Check if k_modelparameter is defined in file NFO
-    if(fModelParameter.empty())  
-    {
-        ERROR<<"Missing information for k_modelparameter in : "<<fTMVANFOFilePath<<endl;
-        exit(1);
-    }
-    //Check if k_predictortype is defined in file NFO
-    if(fPredictorType.empty())  
-    {
-        ERROR<<"Missing information for k_predictortype in : "<<fTMVANFOFilePath<<endl;
-        exit(1);
-    }
-    //Check if k_output is defined in file NFO
-    if(fOutput.empty())  
-    {
-        ERROR<<"Missing information for k_output in : "<<fTMVANFOFilePath<<endl;
-        exit(1);
-    }
-    //Check if k_buffer is defined in file NFO
-    if(fBuffer.empty())  
-    {
-        ERROR<<"Missing information for k_buffer in : "<<fTMVANFOFilePath<<endl;
-        exit(1);
-    }
-    //Check if k_massfractionmin and k_massfractionmax are defined in file NFO
-    if(fStreamListEqMMassFractionMin.empty() || fStreamListEqMMassFractionMax.empty())  
-    {
-        ERROR<<"Missing information for k_massfractionmin and/or k_massfractionmax in : "<<fTMVANFOFilePath<<endl;
-        exit(1);
-    }
-    //Check if k_zainame are defined in file NFO
-    if(fMapOfTMVAVariableNames.empty())  
-    {
-        ERROR<<"Missing information for k_zainame in : "<<fTMVANFOFilePath<<endl;
-        exit(1);
-    }
-    //Check if k_list are defined in file NFO
-    if(fStreamList.empty())  
-    {
-        ERROR<<"Missing information for k_list in : "<<fTMVANFOFilePath<<endl;
-        exit(1);
-    }
+    if(fZAILimits.empty()) {ERROR<<"Missing information for k_zail in : "<<fTMVANFOFilePath<<endl; exit(1);}
+    if(fDBRType.empty()) {ERROR<<"Missing information for k_reactor in : "<<fTMVANFOFilePath<<endl; exit(1);}
+    if(fDBFType.empty()) {ERROR<<"Missing information for k_fuel in : "<<fTMVANFOFilePath<<endl; exit(1);}
+    if(!fSpecificPower) {ERROR<<"Missing information for k_specpower in : "<<fTMVANFOFilePath<<endl; exit(1);}
+    if(!fMaximalBU) {ERROR<<"Missing information for k_maxburnup in : "<<fTMVANFOFilePath<<endl; exit(1);}
+    if(fStreamListEqMMassFractionMin.empty() || fStreamListEqMMassFractionMax.empty()) { ERROR<<"Missing information for k_massfractionmin and/or k_massfractionmax in : "<<fTMVANFOFilePath<<endl; exit(1);}
+    if(fStreamList.empty()) { ERROR<<"Missing information for k_list in : "<<fTMVANFOFilePath<<endl; exit(1); }
+    if(fMapOfTMVAVariableNames.empty()) { ERROR<<"Missing information for k_zainame in : "<<fTMVANFOFilePath<<endl; exit(1);}
+    if(fTargetParameter.empty()) { ERROR<<"Missing information for k_targetparameter in : "<<fTMVANFOFilePath<<endl; exit(1);}
+    if(!fTargetParameterStDev) { ERROR<<"Missing information for fTargetParameterStDev in : "<<fTMVANFOFilePath<<endl; exit(1);}
+    if(fModelParameter.empty()) { ERROR<<"Missing information for k_modelparameter in : "<<fTMVANFOFilePath<<endl; exit(1);}
+    if(fBuffer.empty()) { ERROR<<"Missing information for k_buffer in : "<<fTMVANFOFilePath<<endl; exit(1);}
+    if(fPredictorType.empty()) { ERROR<<"Missing information for k_predictortype in : "<<fTMVANFOFilePath<<endl; exit(1);}
+    if(fOutput.empty()) { ERROR<<"Missing information for k_output in : "<<fTMVANFOFilePath<<endl; exit(1);}
 
     INFO << "__An equivalence model has been define__" << endl;
     INFO << "\tThe TMVA weights file is :" << fTMVAXMLFilePath << endl;
