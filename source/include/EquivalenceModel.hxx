9511ace2452210027ad36a4cbe018d5ec02d5391
Mouginot B <mouginot.baptiste@gmail.com>
2018-02-28 10:38:45 -0600

    readd Nico modifs

--- source/include/EquivalenceModel.hxx
+++ source/include/EquivalenceModel.hxx
@@ -10,7 +10,6 @@
  @author BLG
  @author BaM
  @author FaC
- @author Marc
  @version 3.0
  */
 
@@ -69,143 +68,19 @@ public :
 
 	virtual ~EquivalenceModel();		//!< Destructor
 	//@}
-	
-	/*!
-	 \name Fuel Construction Method
-	 */
-	//@{
-	
-	//{
-	/// BuildFuel function.
-	/*!
-	 Build the fuel following the equivalance model with the proper requierment in term of mass, burnup....
-	 \param double burnup reached by the fuel at the end of irradiation
-	 \param double HMMass, Heavy metal mass needed
-    	 \param map < string , vector <IsotopicVector> > StreamArray, the string is the stream code (fissile fertile ,...) the IsotopicVector the fraction of each IV to take in the (fissile, fertile,..) stock .
-     */
-	virtual	 map <string , vector<double> > BuildFuel(double BurnUp, double HMMass, map < string , vector <IsotopicVector> > StreamArray,  map < string , double> StreamListMassFractionMin, map < string , double> StreamListMassFractionMax, map < int , string> StreamListPriority, map < string , bool> StreamListIsBuffer);
-	//}
-
-
-	//@}
-	TTree* CreateTMVAInputTree(IsotopicVector TheFreshfuel, double ThisTime)	; //!<Create input tmva tree to be read by ExecuteTMVA
-	double CalculateTargetParameter(IsotopicVector TheFuel, string TargetParameterName); //!<Get a fuel parameter associated to the fuel ---> ex : BurnUpMax, keffBOC, keffEOC, ... 
-	double CalculateBurnUpMax(IsotopicVector TheFuel, map<string, double> ModelParameter);//!<Calculate the BU max associated to a fuel composition based on MLP prediction (suitable for PWR)
-	double CalculateKeffAtBOC(IsotopicVector TheFuel); //!<Calculate the keff at BOC associated to a fuel composition based on MLP prediction (suitable for SFR)
-
-    	string GetTMVAXMLFilePath() {return fTMVAXMLFilePath;} // Return the path to TMVA XML File path
-    	string GetTMVANFOFilePath() {return fTMVANFOFilePath;} // Return the path to TMVA NFO File path
-    	void SetTMVAXMLFilePath(string TMVAXMLFilePath) {fTMVAXMLFilePath = TMVAXMLFilePath;} // Set the path to TMVA XML File path
-    	void SetTMVANFOFilePath(string TMVANFOFilePath) {fTMVANFOFilePath = TMVANFOFilePath;} // Set the path to TMVA NFO File path
-
-    /*!
-	 \name Get/Set Method
-	 */
-	//@{
-
-	IsotopicVector GetStreamList(string keyword) {return fStreamList[keyword];}	//!<return the list of ZAI of stream type keyword
-	map < string, IsotopicVector> GetAllStreamList() {return fStreamList;}	 	//!<return all the lists
-
-	int GetStreamListNumber(){return fStreamList.size();};
-	int GetMaxIterration()		 const	{ return fMaxIterration; }		//!< Max iterration in build fueld algorythm	
-	double GetTargetParameterStDev(){return fTargetParameterStDev;}//!< Get the precision on fTargetParameterStDev
-	double GetStreamListEqMMassFractionMax(string keyword){return fStreamListEqMMassFractionMax[keyword] ;}
-	double GetStreamListEqMMassFractionMin(string keyword){return fStreamListEqMMassFractionMin[keyword] ;}
-	double GetPCMPrecision(){return fPCMprecision/1e5;}//!< Get the precision on @f$\langle k \rangle@f$ prediction []. Neural network predictor constructors
-
-    	void SetModelParameter(string sMP, double dMP)  { fModelParameter[sMP] = dMP; }   //!< Set Model Parameters precised in NFO file
-    	map<string, double> GetModelParameter()  { return fModelParameter; }   //!< Get Model Parameters precised in NFO file
-
-	void SetNonZaiTMVAVariable(string snZP, double dnZP);    //!< Set NonZaiTMVAVariables
-	vector< pair<double, string> > GetNonZaiTMVAVariables()  { return fListOfNonZaiTMVAVariables; }   //!< Get NonZaiTMVAVariables
-
-	void SetMaxIterration(int val)	{ fMaxIterration = val; }	//!< Max iteration in build fuel algorithm
-	void SetTargetParameterStDev(double TPSD){fTargetParameterStDev = TPSD;} //!< Set the precision on Target Parameter
-	void SetStreamListEqMMassFractionMax(string keyword, double value){fStreamListEqMMassFractionMax[keyword] = value;}
-	void SetStreamListEqMMassFractionMin(string keyword, double value){fStreamListEqMMassFractionMin[keyword] = value;}
-
-	void SetPCMPrecision(double pcm){fPCMprecision = pcm;}		  //!< Set the precision on @f$\langle k \rangle@f$ prediction [pcm]. Neural network predictor constructors
-
-	/*!
-	 \name Time <-> Burnup conversion
-	 */
-	//@{
-
-	double SecondToBurnup(double Second){return Second*fSpecificPower/(24*3.6e6);}
-	double BurnupToSecond(double BurnUp){return BurnUp/fSpecificPower*(24*3.6e6);}
-
-	//@}
-
-	//@}
-	
-	/*!
-	 \name Reading NFO related Method
-	 */
-	//@{
-	void ReadNFO();
-	virtual void ReadLine(string line);
-
-	
-	virtual void LoadKeyword();
-	void ReadZAIlimits(const string &line);
-	void ReadType(const string &line);
-	
-	//{
-	/// ReadZAIName : read the zai name in the TMWA MLP model
-	/*!
-	 \param line : line suppossed to contain the ZAI name  starts with "k_zainame" keyword
-	 */
-	void ReadZAIName(const string &line);
-	//}
-	
-	//{
-	/// ReadMaxBurnUp : read a guessed (very overestimated) maximum burnup a fuel can reach (purpose : algorithm initialization)
-	/*!
-	 \param line : line suppossed to contain the ZAI name  starts with "k_maxburnup" keyword
-	 */
-	void ReadMaxBurnUp(const string &line);
-	//}
-	
-	//{
-	/// ReadSpecificPower : read the Specific Power of the DataBase
-	/*!
-	 \param line : line suppossed to contain the Specific Power information starts with "k_specpower" keyword
-	 */
-	void ReadSpecificPower(const string &line);
-	//}
 
     map < string, IsotopicVector> GetAllStreamList() {return fStreamList;}		// return all the lists
 
-	//{
-	/// ReadNonZaiTMVAVariables : read the NonZai variables for the predictor (ex : Nbatch, Specific power)
-	/*!
-	 \param line : line suppossed to contain the NonZai variables for TMVA starts with "k_nonZAIforTMVA" keyword
-	 */
-	void ReadNonZaiTMVAVariables(const string &line);
-	//}
-
-	//{
-	/// ReadOutput : read the output type of the predictor (ex : kinf)
-	/*!
-	 \param line : line suppossed to contain the Specific Power information starts with "k_output" keyword
-	 */
-	void ReadOutput(const string &line);
-	//}
+    virtual  map <string , vector<double> > BuildFuel(double BurnUp, double HMMass, map < string , vector <IsotopicVector> > StreamArray,  map < string , double> StreamListMassFractionMin, map < string , double> StreamListMassFractionMax, map < int , string> StreamListPriority, map < string , bool> StreamListIsBuffer);
+    
+	double SecondToBurnup(double Second) {return Second * fSpecificPower / (24 * 3.6e6);}
+	double BurnupToSecond(double BurnUp) {return BurnUp / fSpecificPower * (24 * 3.6e6);}
 
 	bool isIVInDomain(IsotopicVector IV);
 	void StocksTotalMassCalculation(map < string , vector <IsotopicVector> > const& Stocks);
 	void ConvertMassToLambdaVector(string MaterialDenomination, vector<double>& lambda, double MaterialMassNeeded, vector <IsotopicVector>  Stocks);
 
-	string fPredictorType ; 								//!< Type of predictor used in Equivalence Model (ex: MLP)
-	string fOutput ; 								//!< Type of output calculated by the predictor
-	string fBuffer ; 									//!< Name of material used as buffer in fuel 
-
-	map<string, double> fModelParameter ; 					///< Map of equivalence model parameter 
-
- 	vector< pair<double, string> > fListOfNonZaiTMVAVariables ; 					///!<  List of TMVA input variable names that are not ZAIs
- 	map<ZAI,string> fMapOfTMVAVariableNames;				//!<  List of TMVA input variable names (read from fMLPInformationFile ) , name depends on the training step
-
-	double 	fTargetParameterStDev;							//!< Precision on target parameter calculation 
+protected :
 
     map < string, IsotopicVector> fStreamList;                  //!< contains all lists of zai needed to build a fuel (example : 2 -> fissileList+fertileList)
                                                                 //!< each list is identified by a keyword (example : -> "Fissile" & "Fertile")
